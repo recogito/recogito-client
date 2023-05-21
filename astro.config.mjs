6a044ec5eaa7049d0e2e710c6a7129a37f82f29f
Rainer Simon <rainer@rainersimon.io>
2023-05-21 17:29:17 +0200

    Dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server'
+  output: 'server',
+  adapter: netlify()
 });
