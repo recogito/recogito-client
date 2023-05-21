410cd20a45e92ff7949ae49d7ebbd71eca2d7dbc
Rainer Simon <rainer@rainersimon.io>
2023-05-21 17:27:10 +0200

    Added font files

---
+++ src/Types.ts
@@ -0,0 +1,17 @@
+export interface Project {
+
+  id: string;
+
+  created_at: string;
+
+  created_by: string;
+
+  updated_at: string;
+
+  updated_by: string;
+
+  name: string;
+
+  description: string;
+
+}
