55653600d364b16ba1f3e5e522d92a7b91cf7a07
Rainer Simon <rainer@rainersimon.io>
2023-05-22 12:43:57 +0200

    Localization dashboard sidebar

--- src/i18n/de/index.ts
+++ src/i18n/de/index.ts
@@ -1,7 +1,9 @@
 import authLogin from './auth-login.json';
 import dashboardProjects from './dashboard-projects.json';
+import dashboardSidebar from './dashboard-sidebar.json';
 
 export default {
   'auth-login': authLogin,
-  'dashboard-projects': dashboardProjects
+  'dashboard-projects': dashboardProjects,
+  'dashboard-sidebar': dashboardSidebar
 }
--- src/layouts/dashboard/DashboardSidebar.tsx
+++ src/layouts/dashboard/DashboardSidebar.tsx
@@ -1,44 +1,47 @@
 import { ArrowUpRight, SignOut } from '@phosphor-icons/react';
+import type { Translations } from 'src/Types';
 
 import './DashboardSidebar.css';
 
-export const DashboardSidebar = () => {
+export const DashboardSidebar = (props: { i18n: Translations }) => {
+
+  const { i18n } = props;
 
   return (
     <aside className="dashboard-sidebar">
-      <h1>Dashboard</h1>
+      <h1>{i18n['Dashboard']}</h1>
       <nav>
         <ul>
           <li>
-            <h2>Projects</h2>
+            <h2>{i18n['Projects']}</h2>
             <ul>
               <li>
-                <a href="./projects">All projects</a>
+                <a href="./projects">{i18n['All projects']}</a>
               </li>
             </ul>
           </li>
 
           <li>
-            <h2>Account</h2>
+            <h2>{i18n['Account']}</h2>
             <ul>
               <li>
-                <a href="./account/me">Preferences</a>
+                <a href="./account/me">{i18n['Preferences']}</a>
               </li>
             </ul>
           </li>
           
           <li>
-            <h2>Documentation</h2>
+            <h2>{i18n['Documentation']}</h2>
             <ul>
               <li>
                 <a href="./help/tutorial">
-                  <ArrowUpRight className="text-bottom" size={17} /> Tutorial
+                  <ArrowUpRight className="text-bottom" size={17} /> {i18n['Tutorial']}
                 </a>
               </li>
               
               <li>
                 <a href="./helo/faq">
-                  <ArrowUpRight className="text-bottom" size={17} /> FAQ
+                  <ArrowUpRight className="text-bottom" size={17} /> {i18n['FAQ']}
                 </a>
               </li>
             </ul>
@@ -48,7 +51,7 @@ export const DashboardSidebar = () => {
             <ul className="pad">
               <li>
                 <a href="./sign-out">
-                  <SignOut className="text-bottom" size={19} /> Logout
+                  <SignOut className="text-bottom" size={19} /> {i18n['Logout']}
                 </a>
               </li>
             </ul>
