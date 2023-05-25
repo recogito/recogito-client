db02d0eafbb340d420c5c9f45e431c0f12b09bbc
Rainer Simon <rainer@rainersimon.io>
2023-05-25 17:10:22 +0200

    More annotation desktop scaffolding

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import netlify from '@astrojs/netlify/functions';
 
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
--- src/apps/annotation-image/ImageAnnotationDesktop.tsx
+++ src/apps/annotation-image/ImageAnnotationDesktop.tsx
@@ -1,4 +1,41 @@
+import { Annotorious, OpenSeadragonViewer } from '@annotorious/react';
 import type { Translations } from 'src/Types';
+
+const IIIF_SAMPLE = {
+  "@context" : "http://iiif.io/api/image/2/context.json",
+  "protocol" : "http://iiif.io/api/image",
+  "width" : 7808,
+  "height" : 5941,
+  "sizes" : [
+     { "width" : 244, "height" : 185 },
+     { "width" : 488, "height" : 371 },
+     { "width" : 976, "height" : 742 }
+  ],
+  "tiles" : [
+     { "width" : 256, "height" : 256, "scaleFactors" : [ 1, 2, 4, 8, 16, 32 ] }
+  ],
+  "@id" : "https://iiif.bodleian.ox.ac.uk/iiif/image/af315e66-6a85-445b-9e26-012f729fc49c",
+  "profile" : [
+     "http://iiif.io/api/image/2/level2.json",
+     { "formats" : [ "jpg", "png", "webp" ],
+       "qualities" : ["native","color","gray","bitonal"],
+       "supports" : ["regionByPct","regionSquare","sizeByForcedWh","sizeByWh","sizeAboveFull","sizeUpscaling","rotationBy90s","mirroring"],
+       "maxWidth" : 1000,
+       "maxHeight" : 1000
+     }
+  ]
+};
+
+const OSD_OPTIONS = {
+  prefixUrl: 'https://cdn.jsdelivr.net/npm/openseadragon@3.1/build/openseadragon/images/',
+  tileSources: IIIF_SAMPLE,
+  gestureSettingsMouse: {
+    clickToZoom: false
+  }, 
+  showNavigationControl: false
+}
+
+import './ImageAnnotationDesktop.css';
 
 export interface ImageAnnotationDesktopProps {
 
@@ -9,7 +46,13 @@ export interface ImageAnnotationDesktopProps {
 export const ImageAnnotationDesktop = (props: ImageAnnotationDesktopProps) => {
 
   return (
-    <div className="">Image Annotation!</div>
+    <Annotorious>
+      <div className="ia-desktop">
+        <OpenSeadragonViewer
+          className="ia-osd-container"
+          options={OSD_OPTIONS} />
+      </div>
+    </Annotorious>
   )
 
 }
