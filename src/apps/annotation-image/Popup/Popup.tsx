1a5d35a59114012bea74cad9c17fe28f85254b93
Rainer Simon <rainer@rainersimon.io>
2023-05-31 13:03:52 +0200

    Image annotation popup (work in progress)

--- src/apps/annotation-image/Popup/Popup.tsx
+++ src/apps/annotation-image/Popup/Popup.tsx
@@ -1,12 +1,36 @@
-import type { OpenSeadragonPopupProps } from '@annotorious/react';
+import { 
+  OpenSeadragonPopupProps,
+  useAnnotationStore,
+  useAnnotatorUser
+} from '@annotorious/react';
 
 import './Popup.css';
 
 export const Popup = (props: OpenSeadragonPopupProps) => {
 
+  const me = useAnnotatorUser();
+
+  const store = useAnnotationStore();
+
+  // Popup only supports a single selected annotation for now
+  const selected = props.selection[0];
+
+  const { creator } = selected.target;
+
+  const comments = selected.bodies
+    .filter(b => !b.purpose || b.purpose === 'commenting');
+
   return (
-    <article>
-      Hello World
+    <article className="annotation-popup ia-annotation-popup">
+      {comments.length === 0 ? (
+        <header>
+          
+        </header>
+      ) : (
+        <ul>
+
+        </ul>
+      )}
     </article>
   )
 
