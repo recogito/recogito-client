29bcdf06ed224412245fb2277c37e441827873d4
Rainer Simon <rainer@rainersimon.io>
2023-06-04 17:05:44 +0200

    Revisions to empty dashboard prompt

--- src/apps/dashboard-projects/Empty/ProjectsEmpty.tsx
+++ src/apps/dashboard-projects/Empty/ProjectsEmpty.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { RocketLaunch } from '@phosphor-icons/react';
 import type { Translations } from 'src/Types';
+import { Spinner } from '@components/Spinner';
 
 export interface ProjectsEmptyProps {
 
@@ -12,6 +14,16 @@ export interface ProjectsEmptyProps {
 export const ProjectsEmpty = (props: ProjectsEmptyProps) => {
 
   const { t } = props.i18n;
+
+  const [fetching, setFetching] = useState(false);
+
+  const onCreateProject = () => {
+    if (fetching)
+      return;
+
+    setFetching(true);
+    props.onCreateProject();
+  }
   
   return (
     <main className="dashboard-projects-empty">
@@ -20,14 +32,20 @@ export const ProjectsEmpty = (props: ProjectsEmptyProps) => {
           {t['Empty Dashboard? Infinite Possibilities!']}
         </h1>
 
-        <div className="dashboard-projects-empty-cta">
-          <button className="primary" onClick={props.onCreateProject}>
-            <RocketLaunch size={20} /> <span>{t['Start Your First Annotation Project']}</span>
-          </button> 
+        <p>
+          {t['You have no annotation projects.']} <a href="./help/tutorial">{t['Learn more.']}</a>
+        </p>
 
-          <a className="button" href="./help/tutorial">
-            {t['Read the Tutorial']}
-          </a>
+        <div className="dashboard-projects-empty-cta">
+          <button className="primary lg" onClick={onCreateProject}>
+            {fetching ? (
+              <Spinner size={20} />
+            ) : (
+              <>
+                <RocketLaunch size={20} /> <span>{t['Start Your First Annotation Project']}</span>
+              </>
+            )}
+          </button> 
         </div>
       </div>
     </main>
