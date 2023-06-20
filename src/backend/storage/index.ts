eb8ed9a00b5a77938ad3087abe9188e417a1bd73
Rainer Simon <rainer@rainersimon.io>
2023-06-20 15:56:52 +0200

    Cleanup

--- src/backend/storage/index.ts
+++ src/backend/storage/index.ts
@@ -1,127 +1,14 @@
-// import * as tus from 'tus-js-client';
 import Uppy, { UploadResult } from '@uppy/core';
 import XHR from '@uppy/xhr-upload';
 import type { SupabaseClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = `https://${import.meta.env.PUBLIC_SUPABASE}`;
 
-/*
-export const uploadFileResumable = (
-  supabase: SupabaseClient, 
-  file: File, 
-  filename: string,
-  onProgress?: (bytesTotal: number, bytesUploaded: number, percent: number) => void
-): Promise<void> => new Promise((resolve, reject) => {
-  return supabase.auth.getSession().then(({ error, data }) => {
-    if (error) {
-      reject(error)
-    } else {
-      const token = data.session?.access_token;
-      if (!token) {
-        reject('Not authorized');
-      } else {
-        const upload = new tus.Upload(file, {
-          endpoint: `${SUPABASE_URL}/storage/v1/upload/resumable`,
-          retryDelays: [0, 3000, 5000, 10000, 20000],
-          headers: {
-            authorization: `Bearer ${token}`,
-            'x-upsert': 'true'
-          },
-          uploadDataDuringCreation: true,
-          metadata: {
-            bucketName: 'documents',
-            objectName: filename,
-            contentType: file.type
-          },
-          // NOTE: it must be set to 6MB (for now) do not change it
-          // See https://supabase.com/docs/guides/storage/uploads#resumable-upload
-          chunkSize: 6 * 1024 * 1024, 
-          onError: function (error) {
-            reject(error)
-          },
-          onProgress: function (bytesUploaded, bytesTotal) {
-            if (onProgress) {
-              const percent = (bytesUploaded / bytesTotal) * 100;
-              console.log('asdfasddfasdf', percent);
-              onProgress(bytesUploaded, bytesTotal, percent);
-            }
-          },
-          onSuccess: function () {
-            resolve();
-          }
-        });
-
-        return upload.findPreviousUploads().then(previousUploads => {
-          if (previousUploads.length)
-            upload.resumeFromPreviousUpload(previousUploads[0]);
-
-          upload.start();
-        })
-      }
-    }})
-  });
-*/
-
 export const uploadFile = (
-  supabase: SupabaseClient, 
-  file: File, 
-  filename: string
-) => supabase
-      .storage
-      .from('documents')
-      .upload(filename, file);
-
-export const uploadFileWithProgress = (  
-  supabase: SupabaseClient, 
-  file: File, 
-  filename: string,
-  onProgress?: (bytesTotal: number, bytesUploaded: number, percent: number) => void
-): Promise<void> => new Promise((resolve, reject) => {
-  return supabase.auth.getSession()
-    .then(({ error, data }) => {
-      if (error) {
-        reject(error)
-      } else {
-        const token = data.session?.access_token;      
-        if (!token) {
-          reject('Not authorized');
-        } else {
-          // Cf. https://github.com/supabase/storage-api/issues/23#issuecomment-973277262
-          const url = `${SUPABASE_URL}/storage/v1/object/documents/${filename}`;
-
-          const _onError = (evt: ProgressEvent) => 
-            reject(evt);
-
-          const _onSuccess = (evt: ProgressEvent) => 
-            resolve();
-
-          const _onProgress = (evt: ProgressEvent) => {
-            if (onProgress) {
-              const percent = (evt.loaded / evt.total) * 100;
-              onProgress(evt.total, evt.loaded, percent);
-            }
-          }
-          
-          const req = new XMLHttpRequest();
-          req.upload.onabort = _onError;
-          req.upload.onerror = _onError;
-          req.upload.onload = _onSuccess;
-          req.upload.onprogress = _onProgress;
-          req.upload.ontimeout = _onError;
-
-          req.open('POST', url);
-          req.setRequestHeader('authorization', `Bearer ${token}`);
-          req.send(file);
-        }
-      }
-    });
-});
-
-export const uploadFileUppy = (
   supabase: SupabaseClient, 
   file: File,
   name: string,
-  onProgress?: (file: File, progress: number) => void
+  onProgress?: (progress: number) => void
 ): Promise<UploadResult> => new Promise((resolve, reject) => {
   return supabase.auth.getSession().then(({ error, data }) => {
     if (error) {
