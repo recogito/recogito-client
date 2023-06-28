e9d2df8a4293513dd3497edb72e1e6e37a12c000
Rebecca Black <rebecca@performantsoftware.com>
2023-06-28 17:09:51 -0400

    added notification pending alert to projectshome

--- src/apps/dashboard-projects/Grid/ProjectsGrid.tsx
+++ src/apps/dashboard-projects/Grid/ProjectsGrid.tsx
@@ -37,41 +37,25 @@ export const ProjectsGrid = (props: ProjectsGridProps) => {
   }, [props.projects]);
   
   return (
-    <div className="dashboard-projects-home">
-      <header>
-        <nav className="breadcrumbs">
-          <ol>
-            <li>
-              <a href={`/${lang}/projects`}>INeedAName</a>
-            </li>
+    <main>
+      <Button 
+        className="primary" 
+        onClick={onCreateProject}
+        busy={fetching}>
+        <Plus size={20} /> <span>{t['Create New Project']}</span>
+      </Button>
 
-            <li>
-              <a className="breadcrumb-current">{t['Projects']}</a>
-            </li>
-          </ol>
-        </nav>
-      </header>
-
-      <main>
-        <Button 
-          className="primary" 
-          onClick={onCreateProject}
-          busy={fetching}>
-          <Plus size={20} /> <span>{t['Create New Project']}</span>
-        </Button>
-
-        <div className="dashboard-projects-grid">
-          {props.projects.map(project => (
-            <ProjectCard 
-              key={project.id} 
-              i18n={props.i18n}
-              project={project} 
-              onDelete={() => props.onDeleteProject(project)} 
-              onRename={() => props.onRenameProject(project)}/>
-          ))}
-        </div>
-      </main>
-    </div>
+      <div className="dashboard-projects-grid">
+        {props.projects.map(project => (
+          <ProjectCard 
+            key={project.id} 
+            i18n={props.i18n}
+            project={project} 
+            onDelete={() => props.onDeleteProject(project)} 
+            onRename={() => props.onRenameProject(project)}/>
+        ))}
+      </div>
+    </main>
   )
 
 }
