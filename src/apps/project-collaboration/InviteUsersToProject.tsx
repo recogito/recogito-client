0d16c864e74b19dc6e28c33348823bb6e32c94c4
Rebecca Black <rebecca@performantsoftware.com>
2023-07-19 10:27:11 -0400

    fixing how the env variables work

--- src/apps/project-collaboration/InviteUsersToProject.tsx
+++ src/apps/project-collaboration/InviteUsersToProject.tsx
@@ -14,7 +14,7 @@ interface InviteUsersToProjectProps {
 
     project: Project,
 
-    user: UserProfile
+    user: UserProfile | undefined
 
 }
 
@@ -41,7 +41,7 @@ export const InviteUsersToProject = (props: InviteUsersToProjectProps) => {
 
         onSubmit: values => {
             console.log(values);
-            const invited_by_name = user.nickname ? user.nickname : user.last_name ? (user.first_name ? user.first_name + ' ' : '') + user.last_name : undefined;
+            const invited_by_name = user?.nickname ? user.nickname : user?.last_name ? (user.first_name ? user.first_name + ' ' : '') + user.last_name : undefined;
             inviteUserToProject(supabase, values.email, project.id, values.role, invited_by_name, project.name).then((result) => {
                 if (result?.error) {
                   console.error(result.error);
