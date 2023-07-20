731bbb0f02814428a3bf0680964cc6d02e1b987d
Rebecca Black <rebecca@performantsoftware.com>
2023-07-20 19:11:21 -0400

    improved user table on collab page

--- src/apps/project-collaboration/ManageUsers.tsx
+++ src/apps/project-collaboration/ManageUsers.tsx
@@ -1,16 +1,23 @@
 import { getProjectGroups, listProjectUsers, updateUserProjectGroup } from "@backend/crud";
 import { supabase } from "@backend/supabaseBrowserClient";
 import { useEffect, useState } from "react";
-import type { Project, Translations } from "src/Types";
+import type { Project, Translations, UserProfile } from "src/Types";
 import './ManageUsers.css';
 import { ProjectUserRow } from "@components/ProjectUserRow";
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check, X } from '@phosphor-icons/react';
+import * as InviteDialog from '@radix-ui/react-dialog';
+import InviteUsersToProject from "./InviteUsersToProject";
 
+const { Root, Indicator } = Checkbox;
 
 interface ManageUsersProps {
 
     i18n: Translations,
 
-    project: Project
+    project: Project,
+
+    user: UserProfile | undefined
 
 };
 
@@ -20,9 +27,24 @@ export const ManageUsers = (props: ManageUsersProps) => {
 
     const [data, setData] = useState<any[]>();
     const [projectGroups, setProjectGroups] = useState<any[]>([]);
+    const [selected, setSelected] = useState<string[]>([]);
 
     const handleRemoveUser = (id: string) => {
         console.log('user ' + id + ' has been removed');
+    };
+
+    const handleToggleSelected = (id: string) => {
+        if (selected.includes(id)) {
+            setSelected((old) => old.filter((i) => i != id));
+        }
+        else setSelected((old) => [...old, id]);
+    };
+
+    const toggleSelectAll = () => {
+        if (data && data.length == selected.length) {
+            setSelected([]);
+        }
+        else setSelected(data ? data.map((i) => i.profiles.id) : []);
     };
 
     const handleUpdateUser = (userId: string, oldTypeId: string, newTypeId: string) => {
@@ -51,28 +73,59 @@ export const ManageUsers = (props: ManageUsersProps) => {
     return data && data.length && (
         <div className="manage-users">
             <h1>Project Users</h1>
-            <table>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Role</th>
-                </tr>
-                </thead>
-                <tbody>
-                { data?.map((user) => (
-                    <ProjectUserRow
-                        key={user.profiles.id}
-                        i18n={props.i18n}
-                        user={user}
-                        projectId={project.id}
-                        typeId={user.type_id}
-                        projectGroups={projectGroups}
-                        onRemoveUser={() => handleRemoveUser(user.profiles.id)}
-                        onUpdateUser={(typeId: string) => handleUpdateUser(user.profiles.id, user.type_id, typeId)}
-                    />
-                ))}
-                </tbody>
-            </table>
+            <InviteDialog.Root>
+                <InviteDialog.Trigger asChild>
+                    <button className="primary">{t["Invite"]}</button>
+                </InviteDialog.Trigger>
+                <InviteDialog.Portal>
+                    <InviteDialog.Overlay className="InviteDialogOverlay"/>
+                    <InviteDialog.Content className="InviteDialogContent">
+                        <InviteDialog.Title>
+                            Invite User to Project
+                        </InviteDialog.Title>
+                        <InviteDialog.Description>
+                            Enter the email and role below.
+                        </InviteDialog.Description>
+                            <InviteUsersToProject
+                                i18n={props.i18n}
+                                project={props.project}
+                                user={props.user}
+                                />
+                        <InviteDialog.Close asChild>
+                            <button className="InviteDialogClose" aria-label="Close">
+                                <X />
+                            </button>
+                        </InviteDialog.Close>
+                    </InviteDialog.Content>
+                </InviteDialog.Portal>
+            </InviteDialog.Root>
+            <div className="users-table">
+                <div className="header row">
+                    <div style={{ width: '10%' }}>
+                        <Root onCheckedChange={toggleSelectAll} className="CheckboxRoot" checked={data.length == selected.length}>
+                            <Indicator>
+                                <Check size={15} style={{ display: 'flex' }} />
+                            </Indicator>
+                        </Root>
+                    </div>
+                    <div style={{ width: '30%' }}>Name</div>
+                    <div style={{ width: '30%' }}>Role</div>
+                    <div style={{ width: '30%' }}>Actions</div>
+                </div>
+            { data?.map((user) => (
+                <ProjectUserRow
+                    key={user.profiles.id}
+                    i18n={props.i18n}
+                    user={user}
+                    typeId={user.type_id}
+                    onRemoveUser={() => handleRemoveUser(user.profiles.id)}
+                    onUpdateUser={(typeId: string) => handleUpdateUser(user.profiles.id, user.type_id, typeId)}
+                    onSelectRow={() => handleToggleSelected(user.profiles.id)}
+                    selected={selected.includes(user.profiles.id)}
+                    roleName={projectGroups.find((i) => i.id == user.type_id).name}
+                />
+            ))}
+            </div>
         </div>
     );
 }
