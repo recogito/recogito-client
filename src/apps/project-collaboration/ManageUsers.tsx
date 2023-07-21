4967b542c56cdf6805885b8bada7350ccfb2f0b7
Rebecca Black <rebecca@performantsoftware.com>
2023-07-21 16:49:16 -0400

    added edit and remove functionality to users table

--- src/apps/project-collaboration/ManageUsers.tsx
+++ src/apps/project-collaboration/ManageUsers.tsx
@@ -1,4 +1,4 @@
-import { getProjectGroups, listProjectUsers, updateUserProjectGroup } from "@backend/crud";
+import { getProjectGroups, listProjectUsers, removeUserFromProject, updateUserProjectGroup } from "@backend/crud";
 import { supabase } from "@backend/supabaseBrowserClient";
 import { useEffect, useState } from "react";
 import type { Project, Translations, UserProfile } from "src/Types";
@@ -8,6 +8,8 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check, X } from '@phosphor-icons/react';
 import * as InviteDialog from '@radix-ui/react-dialog';
 import InviteUsersToProject from "./InviteUsersToProject";
+import EditUserModal from "./EditUserModal";
+import RemoveUserModal from "./RemoveUserModal";
 
 const { Root, Indicator } = Checkbox;
 
@@ -28,9 +30,20 @@ export const ManageUsers = (props: ManageUsersProps) => {
     const [data, setData] = useState<any[]>();
     const [projectGroups, setProjectGroups] = useState<any[]>([]);
     const [selected, setSelected] = useState<string[]>([]);
+    const [editModalOpen, setEditModalOpen] = useState(false);
+    const [currentlyEditing, setCurrentlyEditing] = useState<{id: string, name: string, type_id: string} | null>(null);
+    const [currentlyRemoving, setCurrentlyRemoving] = useState<{id: string, name: string, typeId: string} | null>(null);
+    const [removeModalOpen, setRemoveModalOpen] = useState(false);
 
-    const handleRemoveUser = (id: string) => {
-        console.log('user ' + id + ' has been removed');
+    const handleRemoveUser = (id: string, typeId: string) => {
+        removeUserFromProject(supabase, id, typeId).then((response) => {
+            if (!response) {
+                setData((old) => old?.filter((i) => i.profiles.id != id));
+                setCurrentlyRemoving(null);
+                setRemoveModalOpen(false);
+            }
+            else console.log(response);
+        });
     };
 
     const handleToggleSelected = (id: string) => {
@@ -47,13 +60,34 @@ export const ManageUsers = (props: ManageUsersProps) => {
         else setSelected(data ? data.map((i) => i.profiles.id) : []);
     };
 
+    const handleOpenEditModal = (id: string, name: string, type_id: string) => {
+        setCurrentlyEditing({ id: id, name: name, type_id: type_id });
+        setEditModalOpen(true);
+    };
+
+    const handleOpenRemoveModal = (id: string, name: string, typeId: string) => {
+        setCurrentlyRemoving({ id: id, name: name, typeId: typeId });
+        setRemoveModalOpen(true);
+        setEditModalOpen(false);
+    };
+
+    const handleCloseRemoveModal = () => {
+        setRemoveModalOpen(false);
+        setCurrentlyRemoving(null);
+        if (currentlyEditing) {
+            setEditModalOpen(true);
+        }
+    };
+
     const handleUpdateUser = (userId: string, oldTypeId: string, newTypeId: string) => {
-        updateUserProjectGroup(supabase, userId, oldTypeId, newTypeId).then(({ error, data }) => {
-            if (data) {
+        updateUserProjectGroup(supabase, userId, oldTypeId, newTypeId).then((response) => {
+            if (!response) {
                 setData((old) => old?.map((row) => (row.type_id == oldTypeId && row.profiles.id == userId) ? { type_id: newTypeId, profiles: row.profiles } : row));
+                setCurrentlyEditing(null);
+                setEditModalOpen(false);
             }
             else {
-                console.log(error);
+                console.log(response);
             }
         });
     };
@@ -78,8 +112,8 @@ export const ManageUsers = (props: ManageUsersProps) => {
                     <button className="primary">{t["Invite"]}</button>
                 </InviteDialog.Trigger>
                 <InviteDialog.Portal>
-                    <InviteDialog.Overlay className="InviteDialogOverlay"/>
-                    <InviteDialog.Content className="InviteDialogContent">
+                    <InviteDialog.Overlay className="CollabDialogOverlay"/>
+                    <InviteDialog.Content className="CollabDialogContent">
                         <InviteDialog.Title>
                             Invite User to Project
                         </InviteDialog.Title>
@@ -92,7 +126,7 @@ export const ManageUsers = (props: ManageUsersProps) => {
                                 user={props.user}
                                 />
                         <InviteDialog.Close asChild>
-                            <button className="InviteDialogClose" aria-label="Close">
+                            <button className="CollabDialogClose" aria-label="Close">
                                 <X />
                             </button>
                         </InviteDialog.Close>
@@ -101,16 +135,17 @@ export const ManageUsers = (props: ManageUsersProps) => {
             </InviteDialog.Root>
             <div className="users-table">
                 <div className="header row">
-                    <div style={{ width: '10%' }}>
-                        <Root onCheckedChange={toggleSelectAll} className="CheckboxRoot" checked={data.length == selected.length}>
+                    <div style={{ width: '5%' }}>
+                        { data && data.length > 1 && (<Root onCheckedChange={toggleSelectAll} className="CheckboxRoot" checked={data.length == selected.length}>
                             <Indicator>
                                 <Check size={15} style={{ display: 'flex' }} />
                             </Indicator>
-                        </Root>
+                        </Root>) }
                     </div>
-                    <div style={{ width: '30%' }}>Name</div>
-                    <div style={{ width: '30%' }}>Role</div>
-                    <div style={{ width: '30%' }}>Actions</div>
+                    <div style={{ width: '20%' }}>Name</div>
+                    <div style={{ width: '30%' }}>Email</div>
+                    <div style={{ width: '18%' }}>Role</div>
+                    <div style={{ width: '27%' }}>Actions</div>
                 </div>
             { data?.map((user) => (
                 <ProjectUserRow
@@ -118,14 +153,31 @@ export const ManageUsers = (props: ManageUsersProps) => {
                     i18n={props.i18n}
                     user={user}
                     typeId={user.type_id}
-                    onRemoveUser={() => handleRemoveUser(user.profiles.id)}
-                    onUpdateUser={(typeId: string) => handleUpdateUser(user.profiles.id, user.type_id, typeId)}
+                    onRemoveUser={() => handleOpenRemoveModal(user.profiles.id, (user.profiles.first_name || user.profiles.last_name) ? (user.profiles.first_name ? `${user.profiles.first_name} ` : '') +user.profiles.last_name : user.profiles.nickname, user.type_id)}
+                    onUpdateUser={(NewTypeId: string) => handleUpdateUser(user.profiles.id, user.type_id, NewTypeId)}
                     onSelectRow={() => handleToggleSelected(user.profiles.id)}
                     selected={selected.includes(user.profiles.id)}
                     roleName={projectGroups.find((i) => i.id == user.type_id).name}
+                    onOpenEditModal={() => handleOpenEditModal(user.profiles.id, (user.profiles.first_name || user.profiles.last_name) ? (user.profiles.first_name ? `${user.profiles.first_name} ` : '') +user.profiles.last_name : user.profiles.nickname, user.type_id)}
                 />
             ))}
             </div>
+            <EditUserModal
+                open={editModalOpen}
+                id={currentlyEditing ? currentlyEditing.id : ''}
+                name={currentlyEditing ? currentlyEditing.name : ''}
+                type_id={currentlyEditing ? currentlyEditing.type_id : ''}
+                projectGroups={projectGroups}
+                onSubmit={(newId) => currentlyEditing && handleUpdateUser(currentlyEditing.id, currentlyEditing.type_id, newId)}
+                onDelete={() => currentlyEditing && handleOpenRemoveModal(currentlyEditing.id, currentlyEditing.name, currentlyEditing.type_id)}
+                onClose={() => { setEditModalOpen(false); setCurrentlyEditing(null) }}
+            />
+            <RemoveUserModal
+                open={removeModalOpen}
+                name={currentlyRemoving ? currentlyRemoving.name : undefined}
+                onConfirm={() => currentlyRemoving && handleRemoveUser(currentlyRemoving.id, currentlyRemoving.typeId)}
+                onClose={handleCloseRemoveModal}
+            />
         </div>
     );
 }
--- src/components/ProjectUserRow/ProjectUserRow.tsx
+++ src/components/ProjectUserRow/ProjectUserRow.tsx
@@ -8,35 +8,42 @@ const { Root, Indicator } = Checkbox;
 interface ProjectUserRowProps {
     i18n: Translations
     user: any
-    typeId: string
+    typeId?: string
+    email?: string
     roleName: string
-    onRemoveUser(): void
-    onUpdateUser: (typeId: string) => void
-    onSelectRow(): void
-    selected: boolean
+    onRemoveUser?: () => void
+    onUpdateUser?: (typeId: string) => void
+    onSelectRow?: () => void
+    onOpenEditModal?: () => void
+    selected?: boolean
+    pending?: boolean
 }
 
 export const ProjectUserRow = (props: ProjectUserRowProps) => {
     const { t } = props.i18n;
-    const { user, typeId, onRemoveUser, onUpdateUser, roleName, onSelectRow, selected } = props;
+    const { user, typeId, onRemoveUser, onUpdateUser, roleName, onSelectRow, selected, onOpenEditModal, pending } = props;
 
     return (
         <div className="row">
-                <div style={{ width: '10%' }}>
-                    <Root onCheckedChange={onSelectRow} className="CheckboxRoot" checked={selected}>
+                <div style={{ width: '5%' }}>
+                    { !pending && (<Root onCheckedChange={onSelectRow} className="CheckboxRoot" checked={selected}>
                         <Indicator>
                             <Check size={15} style={{ display: 'flex' }} />
                         </Indicator>
-                    </Root>
+                    </Root>) }
                 </div>
-                <div style={{ width: '30%' }}>
+                <div style={{ width: '20%' }}>
                     {(user.profiles.first_name || user.profiles.last_name) ? (user.profiles.first_name ? `${user.profiles.first_name} ` : '') +user.profiles.last_name : user.profiles.nickname}
                 </div>
                 <div style={{ width: '30%' }}>
+                    {user.profiles.email}
+                </div>
+                <div style={{ width: '18%' }}>
                     {roleName}
                 </div>
-                <div style={{ width: '30%' }}>
-                    do stuff here!
+                <div style={{ width: '27%', display: 'flex' }}>
+                    <button style={{ display: 'flex', padding: '5px 8px' }} className="primary" onClick={onOpenEditModal}>Edit Role</button>
+                    <button style={{ display: 'flex', padding: '5px 8px' }} onClick={onRemoveUser}>Remove User</button>
                 </div>
             </div>
     )
