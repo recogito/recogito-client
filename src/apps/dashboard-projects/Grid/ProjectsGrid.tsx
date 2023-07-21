ebec8f42b3a1ae13ed1fbc7e7a1ce19db97529f5
Rainer Simon <rainer@rainersimon.io>
2023-07-21 09:31:56 +0200

    Dashboard: notification banner for new invites

--- src/apps/dashboard-projects/Grid/ProjectsGrid.tsx
+++ src/apps/dashboard-projects/Grid/ProjectsGrid.tsx
@@ -47,37 +47,48 @@ export const ProjectsGrid = (props: ProjectsGridProps) => {
   
   return (
     <main>
-      <Button 
-        className="primary" 
-        onClick={onCreateProject}
-        busy={fetching}>
-        <Plus size={20} /> <span>{t['Create New Project']}</span>
-      </Button>
-
-      {props.invitations.length > 0 && (
-        <div className="dashboard-projects-invitations">
-          {props.invitations.map(invitation => (
-            <InvitationCard
-              key={invitation.id}
-              invitation={invitation}
-              i18n={props.i18n} 
-              onAccepted={project => props.onInvitationAccepted(invitation, project)} 
-              onDeclined={() => props.onInvitationDeclined(invitation)} 
-              onError={props.onError} />
-          ))}
-        </div>
+      {props.invitations.length === 0 && (
+        <section className="dashboard-banner info-banner">
+          <h1>2 Invitations</h1>
+          <p>
+            New projects are waiting for you to join!
+          </p>
+        </section>
       )}
 
-      <div className="dashboard-projects-grid">
-        {props.projects.map(project => (
-          <ProjectCard 
-            key={project.id} 
-            i18n={props.i18n}
-            project={project} 
-            onDelete={() => props.onDeleteProject(project)} 
-            onRename={() => props.onRenameProject(project)}/>
-        ))}
-      </div>
+      <section>
+        <Button 
+          className="primary" 
+          onClick={onCreateProject}
+          busy={fetching}>
+          <Plus size={20} /> <span>{t['Create New Project']}</span>
+        </Button>
+
+        {props.invitations.length > 0 && (
+          <div className="dashboard-projects-invitations">
+            {props.invitations.map(invitation => (
+              <InvitationCard
+                key={invitation.id}
+                invitation={invitation}
+                i18n={props.i18n} 
+                onAccepted={project => props.onInvitationAccepted(invitation, project)} 
+                onDeclined={() => props.onInvitationDeclined(invitation)} 
+                onError={props.onError} />
+            ))}
+          </div>
+        )}
+
+        <div className="dashboard-projects-grid">
+          {props.projects.map(project => (
+            <ProjectCard 
+              key={project.id} 
+              i18n={props.i18n}
+              project={project} 
+              onDelete={() => props.onDeleteProject(project)} 
+              onRename={() => props.onRenameProject(project)}/>
+          ))}
+        </div>
+      </section>
     </main>
   )
 
