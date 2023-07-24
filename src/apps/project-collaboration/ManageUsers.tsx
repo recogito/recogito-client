7950bf7c3eeeec7ad0cff4b0f43f0ff3205a24b0
Rebecca Black <rebecca@performantsoftware.com>
2023-07-24 10:23:12 -0400

    fixing something I messed up during rebase

--- src/apps/project-collaboration/ManageUsers.tsx
+++ src/apps/project-collaboration/ManageUsers.tsx
@@ -1,3 +1,108 @@
+import { getProjectGroups, listProjectUsers, removeUserFromProject, updateUserProjectGroup } from "@backend/crud";
+import { supabase } from "@backend/supabaseBrowserClient";
+import { useEffect, useState } from "react";
+import type { Project, Translations, UserProfile } from "src/Types";
+import './ManageUsers.css';
+import { ProjectUserRow } from "@components/ProjectUserRow";
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check, X } from '@phosphor-icons/react';
+import * as InviteDialog from '@radix-ui/react-dialog';
+import InviteUsersToProject from "./InviteUsersToProject";
+import EditUserModal from "./EditUserModal";
+import RemoveUserModal from "./RemoveUserModal";
+
+const { Root, Indicator } = Checkbox;
+
+interface ManageUsersProps {
+
+    i18n: Translations,
+
+    project: Project,
+
+    user: UserProfile | undefined
+
+};
+
+export const ManageUsers = (props: ManageUsersProps) => {
+    const { t } = props.i18n;
+    const { project } = props;
+
+    const [data, setData] = useState<any[]>();
+    const [projectGroups, setProjectGroups] = useState<any[]>([]);
+    const [selected, setSelected] = useState<string[]>([]);
+    const [editModalOpen, setEditModalOpen] = useState(false);
+    const [currentlyEditing, setCurrentlyEditing] = useState<{id: string, name: string, type_id: string} | null>(null);
+    const [currentlyRemoving, setCurrentlyRemoving] = useState<{id: string, name: string, typeId: string} | null>(null);
+    const [removeModalOpen, setRemoveModalOpen] = useState(false);
+
+    const handleRemoveUser = (id: string, typeId: string) => {
+        removeUserFromProject(supabase, id, typeId).then((response) => {
+            if (!response) {
+                setData((old) => old?.filter((i) => i.profiles.id != id));
+                setCurrentlyRemoving(null);
+                setRemoveModalOpen(false);
+            }
+            else console.log(response);
+        });
+    };
+
+    const handleToggleSelected = (id: string) => {
+        if (selected.includes(id)) {
+            setSelected((old) => old.filter((i) => i != id));
+        }
+        else setSelected((old) => [...old, id]);
+    };
+
+    const toggleSelectAll = () => {
+        if (data && data.length == selected.length) {
+            setSelected([]);
+        }
+        else setSelected(data ? data.map((i) => i.profiles.id) : []);
+    };
+
+    const handleOpenEditModal = (id: string, name: string, type_id: string) => {
+        setCurrentlyEditing({ id: id, name: name, type_id: type_id });
+        setEditModalOpen(true);
+    };
+
+    const handleOpenRemoveModal = (id: string, name: string, typeId: string) => {
+        setCurrentlyRemoving({ id: id, name: name, typeId: typeId });
+        setRemoveModalOpen(true);
+        setEditModalOpen(false);
+    };
+
+    const handleCloseRemoveModal = () => {
+        setRemoveModalOpen(false);
+        setCurrentlyRemoving(null);
+        if (currentlyEditing) {
+            setEditModalOpen(true);
+        }
+    };
+
+    const handleUpdateUser = (userId: string, oldTypeId: string, newTypeId: string) => {
+        updateUserProjectGroup(supabase, userId, oldTypeId, newTypeId).then((response) => {
+            if (!response) {
+                setData((old) => old?.map((row) => (row.type_id == oldTypeId && row.profiles.id == userId) ? { type_id: newTypeId, profiles: row.profiles } : row));
+                setCurrentlyEditing(null);
+                setEditModalOpen(false);
+            }
+            else {
+                console.log(response);
+            }
+        });
+    };
+
+    useEffect(() => {
+        getProjectGroups(supabase, project.id).then(({ error, data }) => {
+            if (data) {
+                setProjectGroups(data);
+                listProjectUsers(supabase, data.map((i) => i.id)).then((data) => {console.log(data); setData(data ? data : [])});
+            }
+            else {
+                setData([]);
+            }
+        })
+    }, [])
 
     return data && data.length && (
         <div className="manage-users">
