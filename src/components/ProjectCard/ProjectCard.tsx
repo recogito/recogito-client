310efd9ab32b79ecec56636cdae0588d19d9d0bc
Rainer Simon <rainer@rainersimon.io>
2023-08-02 09:55:20 +0200

    Minor dashboard UI tweaks

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -48,12 +48,8 @@ export const ProjectCard = (props: ProjectCardProps) => {
         {description ? (
           <p>{description}</p>
         ) : (
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <p className="no-description">
+            {props.i18n.t['No description.']}
           </p>
         )}
         <ul className="document-stats">
