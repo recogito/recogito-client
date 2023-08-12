93928dfb3714b03ecc1dc65b37818f3c2c53afe3
Rainer Simon <rainer@rainersimon.io>
2023-08-12 11:00:11 +0200

    Feedback dialog after accepting an invite: state plumbing + component scaffolding

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import * as Popover from '@radix-ui/react-popover';
 import { Bell, X } from '@phosphor-icons/react';
 import type { ExtendedProjectData, Invitation, Translations } from 'src/Types';
@@ -5,6 +6,7 @@ import { EmptyList } from './EmptyList';
 import { InvitationItem } from './InvitationItem';
 
 import './Notifications.css';
+import { InvitationConfirmation } from './InvitiationConfirmation';
 
 const { Close, Content, Portal, Root, Trigger } = Popover;
 
@@ -30,52 +32,68 @@ export const Notifications = (props: NotificationsProps) => {
 
   const count = unhandled.length;
 
+  const [showConfirmation, setShowConfirmation] = useState<Invitation | undefined>(undefined);
+
+  const onAccepted = (invitation: Invitation) => (project: ExtendedProjectData) => {    
+    setShowConfirmation(invitation);
+    props.onInvitationAccepted(invitation, project)
+  }
+
   return (
-    <Root>
-      <Trigger asChild >
-        <button 
-          className="unstyled icon-only notification-actions-trigger actions-trigger"
-          disabled={false}>
+    <>
+      <Root>
+        <Trigger asChild >
+          <button 
+            className="unstyled icon-only notification-actions-trigger actions-trigger"
+            disabled={false}>
 
-          <Bell 
-            size={18} />
+            <Bell 
+              size={18} />
 
-          {Boolean(count) && ( 
-            <div className="pip">{count}</div>
-          )}
-        </button>
-      </Trigger>
-
-      <Portal>
-        <Content className="popover-content no-icons" alignOffset={-20} sideOffset={8} align="end">
-          <section 
-            className={count ? 'notifications' : 'notifications no-pending'}>
-            <header>
-              <h1>{t['Notifications']}</h1>
-              <Close className="unstyled icon-only popover-close" aria-label="Close">
-                <X size={16} />
-              </Close>
-            </header>
-
-            {count === 0 ? (
-              <EmptyList i18n={props.i18n} />
-            ) : (
-              <ol>
-                {props.invitations.map(invitation => (
-                  <InvitationItem 
-                    key={invitation.id}
-                    i18n={props.i18n}
-                    invitation={invitation} 
-                    onAccepted={project => props.onInvitationAccepted(invitation, project)} 
-                    onDeclined={() => props.onInvitationDeclined(invitation)} 
-                    onError={props.onError} />
-                ))}
-              </ol>
+            {Boolean(count) && ( 
+              <div className="pip">{count}</div>
             )}
-          </section>
-        </Content>
-      </Portal>
-    </Root>
+          </button>
+        </Trigger>
+
+        <Portal>
+          <Content className="popover-content no-icons" alignOffset={-20} sideOffset={8} align="end">
+            <section 
+              className={count ? 'notifications' : 'notifications no-pending'}>
+              <header>
+                <h1>{t['Notifications']}</h1>
+                <Close className="unstyled icon-only popover-close" aria-label="Close">
+                  <X size={16} />
+                </Close>
+              </header>
+
+              {count === 0 ? (
+                <EmptyList i18n={props.i18n} />
+              ) : (
+                <ol>
+                  {props.invitations.map(invitation => (
+                    <InvitationItem 
+                      key={invitation.id}
+                      i18n={props.i18n}
+                      invitation={invitation} 
+                      onAccepted={onAccepted(invitation)} 
+                      onDeclined={() => props.onInvitationDeclined(invitation)} 
+                      onError={props.onError} />
+                  ))}
+                </ol>
+              )}
+            </section>
+          </Content>
+        </Portal>
+      </Root>
+
+      {showConfirmation && (
+        <InvitationConfirmation
+          i18n={props.i18n} 
+          invitation={showConfirmation}
+          onClose={() => setShowConfirmation(undefined)} />
+      )}
+    </>
   )
 
 }
