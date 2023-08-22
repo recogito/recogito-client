c224172625ae368b1bd4d6ed2b3249ce416f3347
Rainer Simon <rainer@rainersimon.io>
2023-08-22 10:59:54 +0200

    Removed debug logging

--- src/components/DocumentCard/DocumentCard.tsx
+++ src/components/DocumentCard/DocumentCard.tsx
@@ -24,8 +24,6 @@ export const DocumentCard = (props: DocumentCardProps) => {
 
   const { context, document } = props;
 
-  console.log(document);
-
   const { lang } = props.i18n;
 
   const [editable, setEditable] = useState(false);
