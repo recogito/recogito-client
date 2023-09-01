0e4633aaea50832276b1ee5a857b57d031023e71
Rainer Simon <rainer@rainersimon.io>
2023-09-01 11:09:13 +0200

    Image upload API route now blocked if not logged in + handles basic form validation

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -1,16 +1,27 @@
+import { createSupabaseServerClient } from '@backend/supabaseServerClient';
 import type { APIRoute } from 'astro';
 
 const IIIF_URL = import.meta.env.IIIF_URL;
 const IIIF_PROJECT_ID = import.meta.env.IIIF_PROJECT_ID;
 const IIIF_KEY = import.meta.env.IIIF_KEY;
 
-export const post: APIRoute = async ({ request }) => {
-  // TODO test if the user is authorized
+export const post: APIRoute = async ({ request, cookies }) => {
+  // Verify if the user is logged in
+  const supabase = await createSupabaseServerClient(request, cookies);
+  if (!supabase)
+    return new Response(
+      JSON.stringify({ error: 'Unauthorized'}),
+      { status: 401 });
   
   // Incoming FormData
   const data = await request.formData();
   const name = data.get('name') as string;
   const file = data.get('file') as File;
+
+  if (!(name && file))
+      return new Response(
+        JSON.stringify({ error: 'Bad request'}),
+        { status: 400 });
 
   // Forward as outgoing FormData
   const formData  = new FormData();
