a280bb16589828130cb9298fb300e5e990595cd2
Rainer Simon <rainer@rainersimon.io>
2023-09-05 16:08:51 +0200

    Experimentation: replacing paragraph with an auto-sizing text area for editable comments

--- src/components/Annotation/Comment/PublicComment/PublicComment.tsx
+++ src/components/Annotation/Comment/PublicComment/PublicComment.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import { useAnnotationStore, useAnnotatorUser } from '@annotorious/react';
 import type { PresentUser, User } from '@annotorious/react';
 import { Creator } from '../../Creator';
@@ -10,6 +12,10 @@ export const PublicComment = (props: CommentProps) => {
 
   const { comment, present } = props;
 
+  const textarea = useRef<HTMLTextAreaElement>(null);
+
+  const [editable, setEditable] = useState(true);
+
   const me = useAnnotatorUser();
 
   const store = useAnnotationStore();
@@ -18,6 +24,15 @@ export const PublicComment = (props: CommentProps) => {
     present.find(p => p.id === comment.creator?.id) || comment.creator;
 
   const isMine = creator?.id === me.id;
+
+  const onKeyDown = (evt: React.KeyboardEvent) => {
+    /*
+    if (evt.ctrlKey && evt.key === 'Enter')
+      onSubmit();
+    else if (evt.key === 'Delete')
+      evt.stopPropagation();
+    */
+  }
 
   const onDeleteComment = () => store.deleteBody(comment);
 
@@ -32,7 +47,17 @@ export const PublicComment = (props: CommentProps) => {
         creator={creator} 
         createdAt={comment.created} />
 
-      <p className="no-drag">{comment.value}</p>
+      {editable ? (
+        <TextareaAutosize 
+          className="no-drag"
+          ref={textarea}
+          defaultValue={comment.value}
+          rows={1} 
+          maxRows={10}
+          onKeyDownCapture={onKeyDown} />
+      ) : (
+        <p className="no-drag">{comment.value}</p>
+      )}
 
       {isMine && (
         <PublicCommentActions 
