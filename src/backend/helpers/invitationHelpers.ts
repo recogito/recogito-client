5492243a55321532c8a1db3a0d0901e41f6c3015
Rainer Simon <rainer@rainersimon.io>
2023-09-08 16:14:20 +0200

    Auto-discarding of duplicate invites

--- src/backend/helpers/invitationHelpers.ts
+++ src/backend/helpers/invitationHelpers.ts
@@ -25,6 +25,27 @@ export const joinProject = (
     });
 });
 
+/** 
+ * This method joins a project without retrieving follow-up project data.
+ * We only use this to silently "discard" invitations for project we are
+ * already a member of.
+ */
+export const silentlyJoinProject = (
+  supabase: SupabaseClient,
+  invitation: Invitation
+): Promise<void> => new Promise((resolve, reject) => {
+  supabase
+    .from('invites')
+    .update({ accepted: true })
+    .eq('id', invitation.id)
+    .then(({ error }) => {
+      if (error)
+        reject(error)
+      else
+        resolve();
+    })
+});
+
 export const declineInvitation = (supabase: SupabaseClient, invitation: Invitation) =>
   supabase
     .from('invites')
