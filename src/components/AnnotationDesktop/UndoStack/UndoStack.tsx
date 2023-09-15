8c25747565c0cfe89c1686f5b9085a2ed10c4713
Rainer Simon <rainer@rainersimon.io>
2023-09-15 07:31:36 +0200

    Prevents zombie annotations when selecting a different annotation

--- src/components/AnnotationDesktop/UndoStack/UndoStack.tsx
+++ src/components/AnnotationDesktop/UndoStack/UndoStack.tsx
@@ -43,50 +43,25 @@ export const UndoStack = (props: UndoStackProps) => {
   }, [anno]);
 
   useEffect(() => {
-    if (selected.length === 0 && undoEmpty && created) {
-      // Check if the create annotation is still empty
-      const lastCreated = store.getAnnotation(created.id);
+    if (!undoEmpty || !created)
+      return;
 
-      if (lastCreated.bodies.length === 0) {
-        if (created.bodies.length === 0) {
-          // The problem: deselect will cause the SupabasePlugin 
-          // to store the annotation in the backend, if it changed.
-          // 
-          // In text mode, the annotation is first created (and stored 
-          // in Supabase), then modified as the user drags the selection.
-          // When de-selecting, this change will cause the Supabase 
-          // plugin to trigger a save request.
-          // 
-          // If we delete the annotation in this useEffect handler, this
-          // will lead to the following sequence of events:
-          // 
-          // - The user de-selects
-          // - The @annotorious/core Lifecycle handler is always the first listener on the selection,
-          //   which means that `updateAnnotation` will fire first
-          // - The Supabase plugin starts updating with a preflight OPTION request
-          // - UndoStack gets notfied of the selection change, causing this effect to run
-          // - UndoStack deletes the annotation
-          // - The Supabase plugin's store listener archive-deletes the annotation via the RPC
-          // - The update OPTIONS request returns
-          // - The Supabase plugin sends the PATCH request to update the annotation
-          // - The PATCH request fails because the target was alread archived on Supabase
-          //
-          // The question is how to avoid this situation. One possibility would be to
-          // tweak the Lifcycle handler, to emit the `updateAnnotation` after the event loop
-          // is done. In this case, the sequence will (hopefully...) change to:
-          // 
-          // - The user de-selects
-          // - Undo stack deletes the annotation
-          // - SupabasePlugin archives the annotation
-          // - @annotorious/core lifecycle handler executes, but does not emit a change, 
-          //   because the annotation is already deleted from the store
-          store.deleteAnnotation(lastCreated);
-        }
+    // Don't run for the initial selection of the 'created'
+    // annotation
+    if (selected.length === 1 && selected[0].annotation.id === created.id)
+      return;
+
+    // Check if the create annotation is still empty
+    const lastCreated = store.getAnnotation(created.id);
+
+    if (lastCreated.bodies.length === 0) {
+      if (created.bodies.length === 0) {
+        store.deleteAnnotation(lastCreated);
       }
-
-      setCreated(undefined);
     }
-  }, [selected, created]);
+
+    setCreated(undefined);
+  }, [selected.map(s => s.annotation.id).join('-')]);
 
   return null;
 
