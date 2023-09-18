ac0b30eaaf66b0cc98dea89d7ad3cee9ebeb396e
Rainer Simon <rainer@rainersimon.io>
2023-09-18 08:59:48 +0200

    Selecting an assignment menu action no longer triggers navigation (https://github.com/performant-software/vico/issues/168)

--- src/components/AssignmentCard/AssignmentCardActions.tsx
+++ src/components/AssignmentCard/AssignmentCardActions.tsx
@@ -14,6 +14,17 @@ const { Content, Item, Portal, Root, Trigger } = Dropdown;
 
 export const AssignmentCardActions = (props: AssignmentCardActionsProps) => {
 
+  const stopEvent = (evt: React.MouseEvent) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+  }
+
+  const withStopEvent = (fn: Function) => (evt: Event | React.MouseEvent) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+    fn();
+  }
+
   return (
     <Root>
       <Trigger asChild>
@@ -23,8 +34,12 @@ export const AssignmentCardActions = (props: AssignmentCardActionsProps) => {
       </Trigger>
 
       <Portal>
-        <Content className="dropdown-content no-icons" sideOffset={5} align="start">
-          <Item className="dropdown-item" onSelect={props.onDelete}>
+        <Content 
+          onClick={stopEvent}
+          className="dropdown-content no-icons" 
+          sideOffset={5} 
+          align="start">
+          <Item className="dropdown-item" onSelect={withStopEvent(props.onDelete)}>
             <Trash size={16} /> <span>Delete assignment</span>
           </Item>
         </Content>
