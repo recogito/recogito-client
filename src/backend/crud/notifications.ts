9dd422c53985e75b32fb2ada4164009110efb95d
Rainer Simon <rainer@rainersimon.io>
2023-10-04 21:04:55 +0200

    Made emails lowercase to account for different casing between invite address and user email

--- src/backend/crud/notifications.ts
+++ src/backend/crud/notifications.ts
@@ -4,39 +4,39 @@ import type { Response } from '@backend/Types';
 import { getUser } from '@backend/auth';
 
 export const listMyInvites = (supabase: SupabaseClient): Response<Invitation[]> =>
-    getUser(supabase).then(user => 
-        supabase
-            .from('invites')
-            .select(`
-                id,
-                ignored,
-                created_at,
-                invited_by_name,
-                project_name,
-                project_id
-            `)
-            .is('accepted', false)
-            .eq('email', user?.email)
-            .then(({ error, data }) => ({ error, data: data as Invitation[] })));
+  getUser(supabase).then(user =>
+    supabase
+      .from('invites')
+      .select(`
+        id,
+        ignored,
+        created_at,
+        invited_by_name,
+        project_name,
+        project_id
+      `)
+      .is('accepted', false)
+      .eq('email', user.email?.toLowerCase())
+      .then(({ error, data }) => ({ error, data: data as Invitation[] })));
 
 
 export const processAcceptInvite = (supabase: SupabaseClient, id: string) =>
-        supabase
-            .from('invites')
-            .update({ accepted: true })
-            .eq('id', id)
-            .then(({ error, data }) => { error && console.log(error); return ({ error, data })});
+  supabase
+    .from('invites')
+    .update({ accepted: true })
+    .eq('id', id)
+    .then(({ error, data }) => { error && console.log(error); return ({ error, data }) });
 
 export const processIgnoreInvite = (supabase: SupabaseClient, id: string) =>
-        supabase
-            .from('invites')
-            .update({ ignored: true })
-            .eq('id', id)
-            .then(({ error, data }) => ({ error, data }));
+  supabase
+    .from('invites')
+    .update({ ignored: true })
+    .eq('id', id)
+    .then(({ error, data }) => ({ error, data }));
 
 export const processUnignoreInvite = (supabase: SupabaseClient, id: string) =>
-        supabase
-            .from('invites')
-            .update({ ignored: false })
-            .eq('id', id)
-            .then(({ error, data }) => ({ error, data }));
+  supabase
+    .from('invites')
+    .update({ ignored: false })
+    .eq('id', id)
+    .then(({ error, data }) => ({ error, data }));
