3c1ac4f5b2a5acdacd4e726d9fb1848c1201f655
Rainer Simon <rainer@rainersimon.io>
2023-11-23 09:06:04 +0100

    Changes annotation timestamp format to GoogleDocs-style

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,8 @@ export default defineConfig({
     ssr: {
       noExternal: ['@radix-ui/*', '@phosphor-icons/*'],
     },
+    resolve: {
+      mainFields: [] // react-moment fails without this!
+    }
   },
 });
