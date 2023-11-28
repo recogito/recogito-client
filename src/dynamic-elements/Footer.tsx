43f11c6233ab84fb99a713ad03c256505dc58ac2
lorinjameson <lwjameson@gmail.com>
2023-11-28 15:55:03 -0500

    Removed COVE testing assets

--- src/dynamic-elements/Footer.tsx
+++ src/dynamic-elements/Footer.tsx
@@ -1,16 +1,12 @@
-import COVESingleColor from './COVESingleColor';
-import NEH from './NEH';
+import Test from './Test';
 
 interface FooterProps {
   contrastColor: string;
 }
 const Footer = (props: FooterProps) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <COVESingleColor contrastColor={props.contrastColor} />
-      <div style={{ paddingLeft: 5 }}>
-        <NEH contrastColor={props.contrastColor} href='https://www.neh.gov/' />
-      </div>
+    <div style={{ flexDirection: 'row' }}>
+      <Test contrastColor={props.contrastColor} />
     </div>
   );
 };
