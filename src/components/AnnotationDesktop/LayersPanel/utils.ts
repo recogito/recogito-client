ffe7d788cedd860fed91fcbedf10c83ff80bb440
Rainer Simon <rainer@rainersimon.io>
2023-12-06 10:47:06 +0100

    Bugfix: creators are now properly determined for color-coding and filtering

--- src/components/AnnotationDesktop/LayersPanel/utils.ts
+++ src/components/AnnotationDesktop/LayersPanel/utils.ts
@@ -4,7 +4,7 @@ import type { SupabaseAnnotation } from '@recogito/annotorious-supabase';
 /** Determines the list of unique creators in the given annotation list **/
 export const enumerateCreators = (present: PresentUser[], annotations: SupabaseAnnotation[]) =>
   annotations.reduce((enumerated, a) => {
-    const presentCreator = present.find(p => p.id === a.target.creator?.id || a.target.creator);
+    const presentCreator = present.find(p => p.id === a.target.creator?.id);
     if (presentCreator) {
       const exists = enumerated.find(u => u.id === presentCreator.id);
       return exists ? enumerated : [...enumerated, presentCreator];
