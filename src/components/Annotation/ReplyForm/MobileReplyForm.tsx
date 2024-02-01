5ddb7acb77d6caa39f9f5d42065701470cf3c0d0
Rainer Simon <rainer@rainersimon.io>
2024-02-01 12:07:39 +0100

    Basic component scaffold

--- src/components/Annotation/ReplyForm/MobileReplyForm.tsx
+++ src/components/Annotation/ReplyForm/MobileReplyForm.tsx
@@ -1,21 +1,37 @@
+import { useState } from 'react';
 import { createPortal } from 'react-dom';
 
 import './MobileReplyForm.css';
+import { X } from '@phosphor-icons/react';
 
 export const MobileReplyForm = () => {
+  
+  const [open, setOpen] = useState(false);
 
-  return (
-    <div>Reply...</div>
-  )
+  const onSave = () => {
+    // TODO
+  }
 
-  /*
-  return createPortal(
+  return open ? createPortal(
     <div className="mobile-reply-form">
+      <div className="mobile-reply-form-close">
+        <button onClick={() => setOpen(false)}><X /></button>
+      </div>
+
       <textarea autoFocus/>
+
+      <div className="mobile-reply-form-footer">
+        <button onClick={onSave}>Save</button>
+        <button onClick={() => setOpen(false)}>Cancel</button>
+      </div>
     </div>,
     
     document.body
-  );
-  */
+  ) : (
+    <button onClick={() => setOpen(true)}>
+      Reply...
+    </button>
+  )
+
 
 }
