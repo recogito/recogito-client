7dc3d282e8efcbb22fb91e31b38b76dbb21fd45b
Rainer Simon <rainer@rainersimon.io>
2024-03-19 09:33:20 +0100

    Fixed image upload regression issue

--- src/backend/helpers/documentHelpers.ts
+++ src/backend/helpers/documentHelpers.ts
@@ -3,6 +3,7 @@ import { createDocument, createProjectDocument } from '@backend/crud';
 import { createLayerInContext } from './layerHelpers';
 import { uploadFile, uploadImage } from '@backend/storage';
 import type { Response } from '@backend/Types';
+import { getTagsForContext } from './tagHelpers';
 import type {
   Document,
   DocumentInContext,
@@ -11,7 +12,6 @@ import type {
   Protocol,
   TaggedContext,
 } from 'src/Types';
-import { getTagsForContext } from './tagHelpers';
 
 /**
  * Initializes a new Document in a Context. Process differs for
@@ -44,7 +44,7 @@ export const initDocument = (
         contextId,
         undefined,
         undefined,
-        protocol,
+        'IIIF_IMAGE',
         iiif.manifest_iiif_url
       )
     );
