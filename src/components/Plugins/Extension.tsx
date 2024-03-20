eae469f10f2d2493e47eb9407cd82180247dcbf9
Rainer Simon <rainer@rainersimon.io>
2024-03-20 09:51:37 +0100

    Added note on lazy path limitations

--- src/components/Plugins/Extension.tsx
+++ src/components/Plugins/Extension.tsx
@@ -8,6 +8,10 @@ export const Extension = <T extends ExtensionProps = ExtensionProps>(props: T) =
   const path = plugin.meta.extension_points[extensionPoint];
 
   const ExtensionComponent = useMemo(() => {
+    // Note: although 'lazy' supports variables in the import path, variables
+    // CANNOT CONTAIN A '/' CHARACTER! Runtime loading will fail as soon as the 
+    // variable points to a sub-path!
+    // Cf: https://stackoverflow.com/questions/59051144/dynamic-imports-using-complicated-path-in-react-js
     return lazy(() => import(`../../../plugins/${plugin.meta.directory}/src/${path}/index.js`));
   }, [props.plugin]);
 
