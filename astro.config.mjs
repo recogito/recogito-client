6d2fc6a4cde92842c4da2a8697c35e2107a59543
lorinjameson <lwjameson@gmail.com>
2024-03-22 08:57:37 -0400

    Fixing analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: 'server',
   adapter: netlify(),
   vite: {
