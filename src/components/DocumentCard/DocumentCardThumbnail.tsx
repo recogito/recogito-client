b71e4e086968c113bb8b87bbf7b283861df4d7cb
lorinjameson <lwjameson@gmail.com>
2024-04-18 16:43:13 -0400

    Fix for TEI docs

--- src/components/DocumentCard/DocumentCardThumbnail.tsx
+++ src/components/DocumentCard/DocumentCardThumbnail.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import type { Document } from "src/Types";
+import React, { useState } from 'react';
+import type { Document } from 'src/Types';
 import { ContentTypeIcon } from '@components/DocumentCard/ContentTypeIcon';
 import './DocumentCardThumbnail.css';
 import { Spinner } from '@components/Spinner';
@@ -9,16 +9,15 @@ export interface DocumentCardThumbnailProps {
 }
 
 export const DocumentCardThumbnail = (props: DocumentCardThumbnailProps) => {
-
   const [loading, setLoading] = useState(true);
 
-  const [url, setUrl] = useState<string | null>()
+  const [url, setUrl] = useState<string | null>();
 
   const { document } = props;
 
   const loadManifestThumbnail = (manifestURL: string) => {
     fetch(manifestURL)
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((data) => {
         setLoading(false);
         // Look for thumbnail
@@ -28,41 +27,60 @@ export const DocumentCardThumbnail = (props: DocumentCardThumbnailProps) => {
           setUrl(undefined);
         }
       });
-  }
+  };
 
-  if (document.meta_data?.protocol === 'IIIF_IMAGE') {
-    const url = document.meta_data.url.replace("/info.json", "/square/max/0/default.jpg")
+  if (document.content_type === 'text/plain') {
     return (
-      <div className="document-card-image-container">
-        <img src={url} height={200} width={200} />
-      </div>
-    )
-  } else if (document.content_type === 'text/plain') {
-    return (
-      <div className="document-card-image-container">
+      <div className='document-card-image-container'>
         <img src='/img/text-document.png' height={200} width={200} />
       </div>
-    )
+    );
   } else if (document.content_type == 'application/pdf') {
     return (
-      <div className="document-card-image-container">
+      <div className='document-card-image-container'>
         <img src='/img/pdf-document.png' height={200} width={200} />
       </div>
-    )
+    );
+  } else if (document.content_type == 'text/xml') {
+    return (
+      <div className='document-card-image-container'>
+        <img src='/img/pdf-document.png' height={200} width={200} />
+      </div>
+    );
   } else if (document.meta_data?.protocol === 'IIIF_PRESENTATION') {
     if (loading && !url) {
       loadManifestThumbnail(document.meta_data.url);
     }
     return (
-      <div className="document-card-image-container">
-        {loading ? <div><Spinner className="search-icon spinner" size={14} /></div> : <img src={!loading && url ? url : '/img/iiif-manifest-document.png'} height={200} width={200} />}
+      <div className='document-card-image-container'>
+        {loading ? (
+          <div>
+            <Spinner className='search-icon spinner' size={14} />
+          </div>
+        ) : (
+          <img
+            src={!loading && url ? url : '/img/iiif-manifest-document.png'}
+            height={200}
+            width={200}
+          />
+        )}
       </div>
-    )
+    );
+  } else if (document.meta_data?.protocol === 'IIIF_IMAGE') {
+    const url = document.meta_data.url.replace(
+      '/info.json',
+      '/square/max/0/default.jpg'
+    );
+    return (
+      <div className='document-card-image-container'>
+        <img src={url} height={200} width={200} />
+      </div>
+    );
   } else {
     return (
       <div className='document-card-body'>
         <ContentTypeIcon document={document} />
       </div>
-    )
+    );
   }
-}
+};
