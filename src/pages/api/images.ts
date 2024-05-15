5afbfce04c9087efe852927b4d5cd3577e9ca7cc
lorinjameson <lwjameson@gmail.com>
2024-05-15 10:50:13 -0400

    Logging for image bug

--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -6,39 +6,38 @@ const IIIF_PROJECT_ID = import.meta.env.IIIF_PROJECT_ID;
 const IIIF_KEY = import.meta.env.IIIF_KEY;
 
 export const post: APIRoute = async ({ request, cookies }) => {
+  console.log('Entered api/images POST');
   // Verify if the user is logged in
   const supabase = await createSupabaseServerClient(request, cookies);
   if (!supabase)
-    return new Response(
-      JSON.stringify({ error: 'Unauthorized'}),
-      { status: 401 });
-  
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), {
+      status: 401,
+    });
+
   // Incoming FormData
   const data = await request.formData();
   const name = data.get('name') as string;
   const file = data.get('file') as File;
 
   if (!(name && file))
-      return new Response(
-        JSON.stringify({ error: 'Bad request'}),
-        { status: 400 });
+    return new Response(JSON.stringify({ error: 'Bad request' }), {
+      status: 400,
+    });
 
   // Forward as outgoing FormData
-  const formData  = new FormData();
+  const formData = new FormData();
   formData.append('resource[name]', name);
   formData.append('resource[project_id]', IIIF_PROJECT_ID);
   formData.append('resource[content]', file);
 
+  console.log('Calling IIIF fetch');
   return fetch(IIIF_URL, {
     headers: {
-      'X-API-KEY': IIIF_KEY
+      'X-API-KEY': IIIF_KEY,
     },
     method: 'POST',
-    body: formData
+    body: formData,
   })
-  .then(res => res.json())
-  .then(result => new Response(
-    JSON.stringify(result),
-    { status: 200 }
-  ));
-}
+    .then((res) => res.json())
+    .then((result) => new Response(JSON.stringify(result), { status: 200 }));
+};
