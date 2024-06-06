21e9664d1d9e77bc18a77437ae889202e5e8df9a
Rainer Simon <rainer@rainersimon.io>
2024-06-06 16:26:08 +0200

    Authenticated IIIF thumbnail

---
+++ src/components/DocumentCard/IIIFPresentationThumbnail.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Spinner } from '@components/Spinner';
+
+interface IIIFPresentationThumbnailProps {
+
+  manifestURL: string;
+
+}
+
+export const IIIFPresentationThumbnail = (props: IIIFPresentationThumbnailProps) => {
+
+  const [loading, setLoading] = useState(true);
+
+  const [url, setURL] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (!props.manifestURL) return;
+
+    fetch(props.manifestURL)
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+
+        if (data.thumbnail)
+          setURL(data.thumbnail['@id']);
+      });
+  }, [props.manifestURL]);
+
+  return (
+    <div className='document-card-image-container'>
+      {loading ? (
+        <div className="spinner-container">
+          <Spinner className='search-icon spinner' size={14} />
+        </div>
+      ) : (
+        <img
+          src={url ? url : '/img/iiif-manifest-document.png'}
+          height={200}
+          width={200}
+        />
+      )}
+    </div>
+  );
+
+}
