7f1d07427c4bfb05efe3f77ea717ace587a0379c
Rainer Simon <rainer@rainersimon.io>
2024-06-07 10:10:11 +0200

    Added minimal console log to copy-pdf-worker script

--- copy-pdf-worker.js
+++ copy-pdf-worker.js
@@ -1,4 +1,11 @@
 import fs from 'fs';
 
-const path = './node_modules/@recogito/react-pdf-annotator/dist/pdf.worker.min.mjs';
-fs.copyFileSync(path, './public/pdf.worker.min.mjs');
+console.log('Copying pdf.worker.min.mjs to /public');
+
+try {
+  const path = './node_modules/@recogito/react-pdf-annotator/dist/pdf.worker.min.mjs';
+  fs.copyFileSync(path, './public/pdf.worker.min.mjs');
+} catch (error) {
+  console.error('Error copying pdf.worker.min.mjs');
+  console.error(error);
+}
