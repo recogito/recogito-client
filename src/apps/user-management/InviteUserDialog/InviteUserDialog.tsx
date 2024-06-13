21825a2499fcf49ea4735c7bce64e65e53f02d46
lorinjameson <lwjameson@gmail.com>
2024-06-13 14:48:51 -0400

    Invite Users

---
+++ src/apps/user-management/InviteUserDialog/InviteUserDialog.tsx
@@ -0,0 +1,84 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { X, User } from '@phosphor-icons/react';
+import './InviteUserDialog.css';
+import type { Translations } from 'src/Types';
+import { useState } from 'react';
+
+interface InviteUserDialogProps {
+  i18n: Translations;
+
+  onSave(email: string): void;
+}
+
+export const InviteUserDialog = (props: InviteUserDialogProps) => {
+  const { t } = props.i18n;
+
+  const [email, setEmail] = useState('');
+
+  const validateEmail = (email: string) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <button className='invite-user-button primary'>
+          <User />
+          {t['Invite User']}
+        </button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className='invite-user-dialog-overlay' />
+        <Dialog.Content className='invite-user-dialog-content'>
+          <Dialog.Title className='invite-user-dialog-title'>
+            {t['Invite User']}
+          </Dialog.Title>
+          <Dialog.Description className='invite-user-dialog-description'>
+            {
+              t[
+                'Invite a user to join your organization. They will be sent an email that will allow them to confirm account details.'
+              ]
+            }
+          </Dialog.Description>
+          <fieldset className='invite-user-fieldset'>
+            <label className='invite-user-label' htmlFor='email'>
+              {t['email']}
+            </label>
+            <input
+              className='invite-user-input'
+              id='email'
+              type='email'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </fieldset>
+          <div
+            style={{
+              display: 'flex',
+              marginTop: 25,
+              justifyContent: 'flex-end',
+            }}
+          >
+            <Dialog.Close asChild>
+              <button
+                className='invite-user-button primary'
+                disabled={email.length === 0 || !validateEmail(email)}
+                onClick={() => props.onSave(email)}
+              >
+                {t['Invite']}
+              </button>
+            </Dialog.Close>
+          </div>
+          <Dialog.Close asChild>
+            <button className='invite-user-icon-button' aria-label='Close'>
+              <X size={18} />
+            </button>
+          </Dialog.Close>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
