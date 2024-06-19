9ab7d51441296f47442671ce3a96181ed279febe
Rainer Simon <rainer@rainersimon.io>
2024-06-19 07:53:52 +0200

    Removed unused code

--- src/backend/crud/groupUsers.ts
+++ src/backend/crud/groupUsers.ts
@@ -2,33 +2,6 @@ import type { SupabaseClient } from '@supabase/supabase-js';
 import type { Response } from '@backend/Types';
 import type { GroupMember } from 'src/Types';
 
-/*
-export const getGroupMembers = (
-  supabase: SupabaseClient,
-  groupIds: string[]
-): Response<GroupMember[]> =>
-  supabase
-    .from('group_users')
-    .select(
-      `
-      user:profiles!group_users_user_id_fkey (
-        id,
-        nickname,
-        first_name,
-        last_name,
-        avatar_url
-      ),
-      in_group:type_id,
-      since:created_at
-    `
-    )
-    .in('type_id', groupIds)
-    .then(({ error, data }) => ({
-      error,
-      data: data as unknown as GroupMember[],
-    }));
-*/
-
 export const getProjectGroupMembers = (
   supabase: SupabaseClient,
   groupIds: string[]
