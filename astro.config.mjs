6bd84f79b6adf6da0a52fea836427f9214034db8
Rainer Simon <rainer@rainersimon.io>
2024-06-22 08:28:00 +0200

    Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
   integrations: [
