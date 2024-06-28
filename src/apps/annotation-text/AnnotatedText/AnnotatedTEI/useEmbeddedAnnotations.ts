460e7fd0a227adafec059fd3cd8660cbc247f971
Rainer Simon <rainer@rainersimon.io>
2024-06-28 10:18:47 +0200

    Expanded TEI embedded annotation parsing with user + timestamp info

--- src/apps/annotation-text/AnnotatedText/AnnotatedTEI/useEmbeddedAnnotations.ts
+++ src/apps/annotation-text/AnnotatedText/AnnotatedTEI/useEmbeddedAnnotations.ts
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import type { TextAnnotation } from '@recogito/react-text-annotator';
+import type { TextAnnotation, User } from '@recogito/react-text-annotator';
 import type { EmbeddedLayer } from 'src/Types';
 
 interface AnnotationList {
@@ -10,6 +10,29 @@ interface AnnotationList {
   annotations: TextAnnotation[];
 
 }
+
+interface Note {
+
+  text: string;
+
+  responsible?: User;
+
+}
+
+interface Change {
+
+  who: string;
+
+  when: Date;
+
+  status: 'created' | 'modified';
+
+}
+
+// Shorthands
+const normalizeId = (id?: string | null) => id?.startsWith('#') ? id.substring(1) : id;
+
+const parseDate = (date?: string | null) => date ? new Date(date) : undefined;
 
 export const useEmbeddedTEIAnnotations = (xml?: string) => {
 
@@ -35,15 +58,37 @@ export const useEmbeddedTEIAnnotations = (xml?: string) => {
 
         const [startSelector, endSelector] = el.getAttribute('target')?.split(' ') as [string, string];
 
-        const notes = Array.from(el.querySelectorAll('note')).map(node => node.textContent);
+        const changes = Array.from(el.querySelectorAll('revisionDesc > change')).map(el => ({
+          who: normalizeId(el.getAttribute('who')),
+          when: parseDate(el.getAttribute('when')),
+          status: el.getAttribute('status')
+        }) as Change);
 
-        const tags = Array.from(el.querySelectorAll('rs[ana]')).reduce<string[]>((all, el) => [...all, ...el.getAttribute('ana')!.split(' ')],[]);
+        const users: User[] = Array.from(el.querySelectorAll('respStmt')).map(el => ({
+          id: el.getAttribute('xml:id'),
+          name: el.textContent
+        }) as User);
+
+        const notes: Note[] = Array.from(el.querySelectorAll('note')).map(noteEl => ({
+          text: noteEl.textContent,
+          responsible: users.find(u => u.id === normalizeId(noteEl.getAttribute('resp')))
+        }) as Note);
+
+        const tags: string[] = Array.from(el.querySelectorAll('rs[ana]'))
+          .reduce<string[]>((all, el) => [...all, ...el.getAttribute('ana')!.split(' ')],[]);
+
+        const created = changes.find(c => c.status === 'created');
+        const updated = changes.find(c => c.status === 'modified');
 
         return {
           id,
           layer_id: layerId,
           target: {
             annotation: id,
+            creator: created ? users.find(u => u.id === created.who) : undefined,
+            created: created?.when,
+            updatedBy: updated ? users.find(u => u.id === updated.who) : undefined,
+            updated: updated?.when,
             selector: [{
               startSelector: {
                 type: 'XPathSelector',
@@ -56,16 +101,17 @@ export const useEmbeddedTEIAnnotations = (xml?: string) => {
             }]
           },
           bodies: [
-            ...notes.map(value => ({
+            ...notes.map(note => ({
               id: uuidv4(),
               annotation: id,
-              value
+              value: note.text,
+              creator: note.responsible
             })),
-            ...tags.map(value => ({
+            ...tags.map(tag => ({
               id: uuidv4(),
               annotation: id,
               purpose: 'tagging',
-              value
+              value: tag
             }))
           ]
         } as unknown as TextAnnotation
