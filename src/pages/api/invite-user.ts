47b736d6dd0897b0402c6eecf6e2c57ac5272324
lorinjameson <lwjameson@gmail.com>
2024-07-15 09:07:36 -0400

    Bug logging

--- src/pages/api/invite-user.ts
+++ src/pages/api/invite-user.ts
@@ -8,16 +8,20 @@ const supabaseServerUrl =
 const invitesAllowed = import.meta.env.PUBLIC_ENABLE_USER_INVITE;
 
 export const POST: APIRoute = async ({ request, cookies }) => {
+  console.log('Checking for allowed imports');
   // Should this be callable at all?
   if (!invitesAllowed) {
+    console.log('Invites not allowed!');
     return new Response(JSON.stringify({ error: 'Unauthorized' }), {
       status: 401,
     });
   }
 
   // Verify if the user is logged in
+  console.log('verify login');
   const supabase = await createSupabaseServerClient(cookies);
   if (!supabase) {
+    console.log('Failed to create client!');
     return new Response(JSON.stringify({ error: 'Unauthorized' }), {
       status: 401,
     });
@@ -26,9 +30,11 @@ export const POST: APIRoute = async ({ request, cookies }) => {
   // User must be an Org Admin to use this endpoint
 
   // Get the user
+  console.log('Get User');
   const userResp = await supabase.auth.getUser();
 
   if (userResp.error) {
+    console.log('Get User failed!');
     return new Response(JSON.stringify({ error: 'Failed to get user' }), {
       status: 500,
     });
@@ -39,11 +45,13 @@ export const POST: APIRoute = async ({ request, cookies }) => {
   const id = user.user.id;
 
   // Is the user an Org Admin?
+  console.log('Checking Is org admin');
   const orgAdminResp = await supabase.rpc('is_admin_organization', {
     user_id: id,
   });
 
   if (orgAdminResp.error) {
+    console.log('org admin check failed: ', orgAdminResp.error);
     return new Response(
       JSON.stringify({ error: 'Failed to get org admin response' }),
       {
@@ -53,6 +61,7 @@ export const POST: APIRoute = async ({ request, cookies }) => {
   }
 
   if (!orgAdminResp.data) {
+    console.log('');
     return new Response(JSON.stringify({ error: 'Unauthorized' }), {
       status: 401,
     });
@@ -66,9 +75,11 @@ export const POST: APIRoute = async ({ request, cookies }) => {
 
   const body = await request.json();
 
+  console.log('Email: ', body.email);
   const inviteResp = await supa.auth.admin.inviteUserByEmail(body.email);
 
   if (inviteResp.error) {
+    console.log('Invite error: ', inviteResp.error);
     return new Response(JSON.stringify({ error: 'Failed to invite user' }), {
       status: 500,
     });
