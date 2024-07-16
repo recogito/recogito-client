842d00a88365f05e8c7d91b91f4c736739db5e07
Rainer Simon <rainer@rainersimon.io>
2024-07-16 08:20:35 +0200

    Fixed breaking changes in response to auth-helpers-shared 0.7 upgrade

--- src/backend/supabaseServerClient.ts
+++ src/backend/supabaseServerClient.ts
@@ -1,9 +1,7 @@
 import type { SupabaseClient } from '@supabase/supabase-js';
 import type { AstroCookies } from 'astro';
 import {
-  type CookieOptions,
-  createBrowserSupabaseClient as _createBrowserSupabaseClient,
-  createServerSupabaseClient as _createServerSupabaseClient,
+  createSupabaseClient as _createSupabaseClient,
   type SupabaseClientOptionsWithoutAuth
 } from '@supabase/auth-helpers-shared';
 
@@ -11,64 +9,72 @@ const supabaseServerUrl =
   import.meta.env.SUPABASE_SERVERCLIENT_URL ||
   import.meta.env.PUBLIC_SUPABASE;
 
+const supabaseAPIKey =
+  import.meta.env.PUBLIC_SUPABASE_API_KEY;
+
 export async function createSupabaseServerClient(
   request: Request,
   cookies: AstroCookies,
   {
     supabaseUrl,
     supabaseKey,
-    options,
-    cookieOptions
+    options
   }: {
     supabaseUrl: string;
     supabaseKey: string;
     options?: SupabaseClientOptionsWithoutAuth;
-    cookieOptions?: CookieOptions;
   } = {
     supabaseUrl: supabaseServerUrl,
-    supabaseKey: import.meta.env.PUBLIC_SUPABASE_API_KEY,
-    //@ts-ignore
-    cookieOptions: {
-      name: 'sb-auth-token'
-    }
+    supabaseKey: supabaseAPIKey
   }
 ) {
 
-  const client = _createServerSupabaseClient({
+  const client = _createSupabaseClient(
     supabaseUrl,
     supabaseKey,
-    getRequestHeader: (key) => request.headers.get(key) as (string | undefined),
-
-    getCookie(name) {
-      return cookies.get(name)?.value;
-    },
-
-    setCookie(name, value, options) {
-      cookies.set(name, value, options);
-    },
-
-    options: {
+    {
       ...options,
       global: {
         ...options?.global,
         headers: {
           ...options?.global?.headers
         }
-      }
-    },
-    cookieOptions
-  });
+      },
+      auth: {
+        storage: {
+          // getRequestHeader: (key: string) => request.headers.get(key) as (string | undefined),
 
-  const success = await refreshSession(client, cookies);
+          getItem: (name: string) => {
+            return cookies.get(name)?.value as string;
+          },
+
+          setItem: (name: string, value: string) => {
+            cookies.set(name, value);
+          },
+
+          removeItem: (name: string) => {
+            cookies.delete(name)
+          }
+        }
+      }
+    }
+  );
+
+  /* const success = */ await refreshSession(client, cookies);
   // return success ? client : null;
   return client;
 }
 
 const refreshSession = async (supabase: SupabaseClient, cookies: AstroCookies) => {  
-  const { data: { session }} = await supabase.auth.getSession();
-  if (session)
-    return true;
+  console.log('checking session');
 
+  const { data: { session }} = await supabase.auth.getSession();
+  if (session) {
+    console.log('session is valid')
+    return true;
+  }
+
+  console.log('refreshing...')
   const refreshToken = cookies.get('sb-refresh-token');
   const accessToken = cookies.get('sb-access-token');
 
@@ -76,6 +82,17 @@ const refreshSession = async (supabase: SupabaseClient, cookies: AstroCookies) =
     return await supabase.auth.setSession({ 
       refresh_token: refreshToken.value, 
       access_token: accessToken.value 
-    }).then(({ error }) => !error)
+    }).then(({ error }) => {
+      console.log('error?', error);
+      return !error
+    }).catch(error => {
+      console.log('Error: could not set auth session', error);
+    })
+  } else {
+    if (!refreshToken?.value)
+      console.log('could not find refresh token');
+
+    if (!accessToken?.value)
+      console.log('could not find access token');
   }
 }
