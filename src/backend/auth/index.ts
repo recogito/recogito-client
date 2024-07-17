82ec0d586c953ab2d6a16cb823b52710d514c19f
Rainer Simon <rainer@rainersimon.io>
2024-07-17 11:10:17 +0200

    Added logging to login flow

--- src/backend/auth/index.ts
+++ src/backend/auth/index.ts
@@ -1,17 +1,24 @@
 import type { SupabaseClient, User } from '@supabase/supabase-js';
 
-export const getUser = (supabase: SupabaseClient): Promise<User> =>
-  supabase.auth.getUser().then(({ error, data: { user } }) => {
+export const getUser = (supabase: SupabaseClient): Promise<User> => {
+  console.log('calling supabase.auth.getUser()');
+  return supabase.auth.getUser().then(({ error, data: { user } }) => {
     if (error) {
+      console.error('error getting user');
+      console.error(error);
       throw error;
     } else if (user === null) {
+      console.error('No error, but could not get user object');
       throw 'Unauthorized';
     } else if (user.role !== 'authenticated') {
+      console.error('User not authenticated', user);
       throw 'Unauthorized';
     } else {
+      console.log('success', user);
       return user;
     }
   });
+}
 
 export const isLoggedIn = (supabase: SupabaseClient) =>
   getUser(supabase)
