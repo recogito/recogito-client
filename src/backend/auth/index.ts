7ad408fb3d0fe192031a05ca6042bcc11b118582
Rainer Simon <rainer@rainersimon.io>
2024-07-17 11:47:50 +0200

    Replacing the user of auth.getUser() with auth.getSession()

--- src/backend/auth/index.ts
+++ src/backend/auth/index.ts
@@ -1,15 +1,22 @@
 import type { SupabaseClient, User } from '@supabase/supabase-js';
 
 export const getUser = (supabase: SupabaseClient): Promise<User> =>
-  supabase.auth.getUser().then(({ error, data: { user } }) => {
+  supabase.auth.getSession().then(({ error, data }) => {
     if (error) {
+      console.error('error getting session');
+      console.error(error);
       throw error;
-    } else if (user === null) {
-      throw 'Unauthorized';
-    } else if (user.role !== 'authenticated') {
+    } else if (!data?.session) {
+      console.error('No error, but could not get session object');
       throw 'Unauthorized';
     } else {
-      return user;
+      const { user } = data.session;
+      if (!user || user.role !== 'authenticated') {
+        console.error('User not authenticated', user);
+        throw 'Unauthorized';
+      } else {
+        return user;
+      }
     }
   });
 
