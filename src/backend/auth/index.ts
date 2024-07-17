cfd64f070cc52e80aac62cf85fbc837030381f40
Rainer Simon <rainer@rainersimon.io>
2024-07-17 13:59:38 +0200

    Re-added call to supabase.auth.getUser(), but only after supabase.auth.getSession()

--- src/backend/auth/index.ts
+++ src/backend/auth/index.ts
@@ -1,24 +1,34 @@
 import type { SupabaseClient, User } from '@supabase/supabase-js';
 
-export const getUser = (supabase: SupabaseClient): Promise<User> =>
-  supabase.auth.getSession().then(({ error, data }) => {
+export const getUser = (supabase: SupabaseClient): Promise<User> => {
+  console.log('calling supabase.auth.getSession()');
+
+  return supabase.auth.getSession().then(({ error }) => {
     if (error) {
       console.error('error getting session');
       console.error(error);
       throw error;
-    } else if (!data?.session) {
-      console.error('No error, but could not get session object');
-      throw 'Unauthorized';
     } else {
-      const { user } = data.session;
-      if (!user || user.role !== 'authenticated') {
-        console.error('User not authenticated', user);
-        throw 'Unauthorized';
-      } else {
-        return user;
-      }
+      console.log('calling supabase.auth.getUser()');
+
+      return supabase.auth.getUser().then(({ error, data }) => {
+        if (error) {
+          console.error('error getting user');
+          console.error(error);
+          throw error;
+        } else if (!data.user) {
+          console.error('No error, but could not get session object');
+          throw 'Unauthorized';
+        } else if (data.user.role !== 'authenticated') {
+          console.error('User not authenticated', data.user);
+          throw 'Unauthorized';
+        } else {
+          return data.user;
+        }
+      });
     }
   });
+}
 
 export const isLoggedIn = (supabase: SupabaseClient) =>
   getUser(supabase)
