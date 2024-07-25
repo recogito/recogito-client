d500cfbf3e7a2bd6eb011d7b740118bea832c2e6
Rainer Simon <rainer@rainersimon.io>
2024-07-25 09:41:03 +0200

    Account Setting page facelift

--- src/apps/dashboard-account/AccountPreferences.tsx
+++ src/apps/dashboard-account/AccountPreferences.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useFormik } from 'formik';
-import { ArrowLeft } from '@phosphor-icons/react';
 import { updateMyProfile } from '@backend/crud/profiles';
 import { supabase } from '@backend/supabaseBrowserClient';
 import { Avatar } from '@components/Avatar';
@@ -24,7 +23,7 @@ interface AccountPreferencesProps {
 
 export const AccountPreferences = (props: AccountPreferencesProps) => {
 
-  const { lang, t } = props.i18n;
+  const { t } = props.i18n;
 
   const { profile } = props;
 
@@ -61,14 +60,17 @@ export const AccountPreferences = (props: AccountPreferencesProps) => {
             type: 'error' 
           });
         } else {
-          setError({ 
-            title: t['Profile updated'], 
-            type: 'success' 
-          });
+          // Note: unlike window.history.back(), this also reloads the page.
+          // If we use .back(), the profile nag dialog would pop up again
+          // even if the user has just set a name.
+          window.location.href = document.referrer;
         }
       });
     }
   });
+
+  const onCancel = () => 
+    window.location.href = document.referrer;
 
   return (
     <ToastProvider>
@@ -81,9 +83,6 @@ export const AccountPreferences = (props: AccountPreferencesProps) => {
           showNotifications={true} />
 
         <main>
-          <a href={`/${lang}/projects`}>
-            <ArrowLeft className="text-bottom" size={16} /><span>Back</span>
-          </a>
           <h1>{t['Your User Profile']}</h1>
           <form onSubmit={formik.handleSubmit}>
             <fieldset>
@@ -116,9 +115,10 @@ export const AccountPreferences = (props: AccountPreferencesProps) => {
             </fieldset>
 
             <h2>{t['Public Information']}</h2>
-            <span>
+
+            <p className="public-information-hint">
               {t['Other users can see this information about you. All fields are optional.']}
-            </span>
+            </p>
 
             <fieldset>
               <div className="field">
@@ -170,7 +170,20 @@ export const AccountPreferences = (props: AccountPreferencesProps) => {
               </div>
             </fieldset>
 
-            <button className="primary" type="submit">Submit</button>
+            <div className="actions">
+              <button 
+                className="unstyled"
+                type="button"
+                onClick={onCancel}>
+                {t['Cancel']}
+              </button>
+              
+              <button 
+                className="primary" 
+                type="submit">
+                {t['Save']}
+              </button>
+            </div>
           </form>
         </main>
       </div>
