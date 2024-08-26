c742cb2fa13026f437b4ed18e2214debae6bb817
dleadbetter <derek@performantsoftware.com>
2024-08-26 13:52:16 -0400

    RS #306 - Refactoring LoginLayout image URL

--- src/components/Branding/BrandHeader.tsx
+++ src/components/Branding/BrandHeader.tsx
@@ -1,24 +1,23 @@
 import config from 'src/config.json';
 import './BrandHeader.css';
 
-export const BrandHeader = (props) => {
-  const { site_name: siteName } = config.branding;
+const { site_name: siteName, top_logo: topLogo } = config.branding;
+const imageUrl = `/img/${topLogo || 'recogito-studio-logo.svg'}`;
 
-  return (
-    <header
-      className='brand-header'
-      {...props}
+export const BrandHeader = (props) => (
+  <header
+    className='brand-header'
+    {...props}
+  >
+    <div
+      className='brand-header-container'
     >
-      <div
-        className='brand-header-container'
-      >
-        <img
-          height={40}
-          src='/img/branding/login/header-logo.svg'
-        />
-        <div className='separator'></div>
-        <div className='site-name'>{siteName}</div>
-      </div>
-    </header>
-  );
-};
+      <img
+        height={40}
+        src={imageUrl}
+      />
+      <div className='separator'></div>
+      <div className='site-name'>{siteName}</div>
+    </div>
+  </header>
+);
