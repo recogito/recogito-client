cb482ee8a66c0ae1ff2673fd59bf44bf90b4fd78
lorinjameson <lwjameson@gmail.com>
2024-10-04 11:18:04 -0400

    Fix for logout

--- src/apps/auth-logout/Logout.tsx
+++ src/apps/auth-logout/Logout.tsx
@@ -11,15 +11,14 @@ const clearCookies = () => {
   document.cookie = `sb-auth-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
   document.cookie = `sb-access-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
   document.cookie = `sb-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
-}
+};
 
 export const Logout = (props: { i18n: Translations }) => {
-
   const [error, setError] = useState(false);
 
   useEffect(() => {
     supabase.auth.signOut().then(({ error }) => {
-      if (error) {
+      if (error && error?.status !== 403) {
         setError(true);
       } else {
         clearCookies();
@@ -29,13 +28,13 @@ export const Logout = (props: { i18n: Translations }) => {
   }, []);
 
   return error ? (
-    <div className="logout logout-error">
-      <SmileySad className="text-bottom" size={24} /> {props.i18n.t['Something went wrong.']}
+    <div className='logout logout-error'>
+      <SmileySad className='text-bottom' size={24} />{' '}
+      {props.i18n.t['Something went wrong.']}
     </div>
   ) : (
-    <div className="logout logout-processing">
+    <div className='logout logout-processing'>
       <Spinner size={24} />
     </div>
-  )
-
-}
+  );
+};
