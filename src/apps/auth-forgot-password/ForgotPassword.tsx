06ea9e691b610ea0de8590405a7fd244389a0de1
lorinjameson <lwjameson@gmail.com>
2024-11-21 14:17:27 -0500

    Correct host in reset

--- src/apps/auth-forgot-password/ForgotPassword.tsx
+++ src/apps/auth-forgot-password/ForgotPassword.tsx
@@ -9,18 +9,18 @@ import type { Translations } from 'src/Types';
 import './ForgotPassword.css';
 
 interface ForgotPasswordProps {
-
   i18n: Translations;
-
 }
 
 // https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
-export const isValidEmail = (email: string) => 
-  email.toLowerCase()
-    .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+export const isValidEmail = (email: string) =>
+  email
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 
 export const ForgotPassword = (props: ForgotPasswordProps) => {
-
   const { t } = props.i18n;
 
   const [email, setEmail] = useState('');
@@ -37,10 +37,14 @@ export const ForgotPassword = (props: ForgotPasswordProps) => {
     if (isValidEmail(email)) {
       setBusy(true);
 
-      const { origin } = window.location;
-      const redirectTo = `${origin}/reset-password`;
+      const host =
+        window.location.port !== ''
+          ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
+          : `${window.location.protocol}//${window.location.hostname}`;
+      const redirectTo = `${host}/reset-password`;
 
-      supabase.auth.resetPasswordForEmail(email, { redirectTo })
+      supabase.auth
+        .resetPasswordForEmail(email, { redirectTo })
         .then(({ error }) => {
           if (error) {
             console.error(error);
@@ -48,60 +52,60 @@ export const ForgotPassword = (props: ForgotPasswordProps) => {
           } else {
             setSent(true);
           }
-  
+
           setBusy(false);
         });
     } else {
       setError('Please enter a valid email');
     }
-  }
+  };
 
   return (
-    <div className="forgot-password">
+    <div className='forgot-password'>
       {sent ? (
         <main>
-          <div className="success">
+          <div className='success'>
             <AnimatedCheck size={38} />
-            <p>
-              {t['Link sent']}
-            </p>
+            <p>{t['Link sent']}</p>
           </div>
         </main>
       ) : (
         <main>
           <h1>{t['Forgot Password?']}</h1>
-          <p>
-            {t['Please enter the email you use to sign in.']}
-          </p>
+          <p>{t['Please enter the email you use to sign in.']}</p>
 
           <form className='login'>
             <TextInput
               autoComplete={false}
               error={Boolean(error)}
-              id="email"
-              name="email" 
+              id='email'
+              name='email'
               label={t['Your email']}
-              className="lg w-full" 
-              onChange={setEmail} />
+              className='lg w-full'
+              onChange={setEmail}
+            />
 
             {error && (
-              <p className="error">
-                <WarningOctagon 
-                  className="icon text-bottom" 
-                  size={18} weight="fill" /> {error}
+              <p className='error'>
+                <WarningOctagon
+                  className='icon text-bottom'
+                  size={18}
+                  weight='fill'
+                />{' '}
+                {error}
               </p>
             )}
 
             <Button
               busy={busy}
-              className="primary lg w-full"
-              onClick={onRequestReset}>
+              className='primary lg w-full'
+              onClick={onRequestReset}
+            >
               <span>{t['Request password reset']}</span>
             </Button>
           </form>
         </main>
       )}
     </div>
-  )
-
-}
+  );
+};
