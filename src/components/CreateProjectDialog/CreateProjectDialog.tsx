65b0a1b444109833f3e38f0fbc64963bfd35ec68
lorinjameson <lwjameson@gmail.com>
2024-11-22 14:57:40 -0500

    WIP

--- src/components/CreateProjectDialog/CreateProjectDialog.tsx
+++ src/components/CreateProjectDialog/CreateProjectDialog.tsx
@@ -41,109 +41,111 @@ export const CreateProjectDialog = (props: CreateProjectDialogProps) => {
       <Dialog.Portal>
         <Dialog.Overlay className='dialog-overlay' />
 
-        <Dialog.Content className='dialog-content'>
-          <Dialog.Title className='dialog-title'>
-            {t['Create Project']}
-          </Dialog.Title>
-          <div className='create-project-root'>
-            <Label.Root
-              className='create-project-label-detail text-body-large-bold'
-              htmlFor='firstName'
-            >
-              {t['Project Details']}
-            </Label.Root>
-            <Label.Root className='create-project-label text-body-small-bold'>
-              {t['Name']}
-            </Label.Root>
-            <input
-              className='create-project-input'
-              type='text'
-              value={name}
-              placeholder={t['Name your project']}
-              onChange={(evt) => setName(evt.target.value)}
-            />
-            <Label.Root className='create-project-label text-body-small-bold'>
-              {t['Description']}
-            </Label.Root>
-            <input
-              type='text'
-              value={description}
-              placeholder={t['Describe your project']}
-              onChange={(evt) => setDescription(evt.target.value)}
-            />
-            <div className='create-project-visibility'>
+        <Dialog.Content className='dialog-content-alt'>
+          <div className='create-project-container'>
+            <Dialog.Title className='dialog-title'>
+              {t['Create Project']}
+            </Dialog.Title>
+            <div className='create-project-root'>
               <Label.Root
                 className='create-project-label-detail text-body-large-bold'
                 htmlFor='firstName'
               >
-                {t['Project Visibility']}
+                {t['Project Details']}
               </Label.Root>
-              <div className='create-project-switches'>
-                <RadioGroup.Root
-                  className='create-project-radio-group-root'
-                  defaultValue='private'
-                  value={visibility}
-                  aria-label='View density'
-                  onValueChange={(value) =>
-                    value === 'public' ? setOpenJoin(true) : setOpenJoin(false)
-                  }
+              <Label.Root className='create-project-label text-body-small-bold'>
+                {t['Name']}
+              </Label.Root>
+              <input
+                className='create-project-input'
+                type='text'
+                value={name}
+                placeholder={t['Name your project']}
+                onChange={(evt) => setName(evt.target.value)}
+              />
+              <Label.Root className='create-project-label text-body-small-bold'>
+                {t['Description']}
+              </Label.Root>
+              <input
+                type='text'
+                value={description}
+                placeholder={t['Describe your project']}
+                onChange={(evt) => setDescription(evt.target.value)}
+              />
+              <div className='create-project-subgroup'>
+                <Label.Root
+                  className='create-project-label-detail text-body-large-bold'
+                  htmlFor='firstName'
                 >
-                  <div className='create-project-radio-group-item'>
-                    <RadioGroup.Item
-                      className='create-project-radio-group-button'
-                      value='private'
-                      id='r1'
-                    >
-                      <RadioGroup.Indicator className='create-project-radio-group-indicator' />
-                    </RadioGroup.Item>
-                    <div className='create-project-label-container'>
+                  {t['Project Visibility']}
+                </Label.Root>
+                <div className='create-project-switches'>
+                  <RadioGroup.Root
+                    className='create-project-radio-group-root'
+                    defaultValue='private'
+                    value={visibility}
+                    aria-label='View density'
+                    onValueChange={(value) =>
+                      value === 'public'
+                        ? setOpenJoin(true)
+                        : setOpenJoin(false)
+                    }
+                  >
+                    <div className='create-project-radio-group-item'>
+                      <RadioGroup.Item
+                        className='create-project-radio-group-button'
+                        value='private'
+                        id='r1'
+                      >
+                        <RadioGroup.Indicator className='create-project-radio-group-indicator' />
+                      </RadioGroup.Item>
                       <label
                         className='create-project-radio-group-label text-body-small-bold'
                         htmlFor='r1'
                       >
                         {t['Private']}
                       </label>
-                      <div className='create-project-radio-group-helper text-body-small'>
-                        {
-                          t[
-                            'Project admins choose the users that can join this project'
-                          ]
-                        }
-                      </div>
                     </div>
-                  </div>
-                  <div className='create-project-radio-group-item'>
-                    <RadioGroup.Item
-                      className='create-project-radio-group-button'
-                      value='public'
-                      id='r2'
-                    >
-                      <RadioGroup.Indicator className='create-project-radio-group-indicator' />
-                    </RadioGroup.Item>
-                    <div className='create-project-label-container'>
+                    <div className='create-project-radio-group-helper text-body-small'>
+                      {
+                        t[
+                          'Project admins choose the users that can join this project'
+                        ]
+                      }
+                    </div>
+                    <div className='create-project-radio-group-item'>
+                      <RadioGroup.Item
+                        className='create-project-radio-group-button'
+                        value='public'
+                        id='r2'
+                      >
+                        <RadioGroup.Indicator className='create-project-radio-group-indicator' />
+                      </RadioGroup.Item>
                       <label
                         className='create-project-radio-group-label text-body-small-bold'
                         htmlFor='r2'
                       >
                         {t['Public']}
                       </label>
-                      <div className='create-project-radio-group-helper text-body-small'>
-                        {
-                          t[
-                            'Any registered user can join this project without an invitation'
-                          ]
-                        }
-                      </div>
                     </div>
-                  </div>
-                </RadioGroup.Root>
-                <div className='create-project-switches lower'>
-                  <Label.Root
-                    className='create-project-label-detail text-body-large-bold'
-                    htmlFor='firstName'
-                  >
-                    {t['Project Type']}
-                  </Label.Root>
+                    <div className='create-project-radio-group-helper text-body-small'>
+                      {
+                        t[
+                          'Any registered user can join this project without an invitation'
+                        ]
+                      }
+                    </div>
+                  </RadioGroup.Root>
+                </div>
+              </div>
+              <div className='create-project-subgroup'>
+                <Label.Root
+                  className='create-project-label-detail text-body-large-bold'
+                  htmlFor='firstName'
+                >
+                  {t['Project Type']}
+                </Label.Root>
+                <div className='create-project-switches'>
                   <RadioGroup.Root
                     className='create-project-radio-group-root lower'
                     defaultValue='assignments'
@@ -202,20 +204,23 @@ export const CreateProjectDialog = (props: CreateProjectDialogProps) => {
                   </RadioGroup.Root>
                 </div>
               </div>
-              <div className='create-project-buttons dialog-action'>
-                <Button type='button' className='sm' onClick={props.onClose}>
-                  {t['Cancel']}
-                </Button>
-
-                <Button
-                  disabled={name.length === 0 || description.length === 0}
-                  className='primary sm'
-                  onClick={handleCreateProject}
-                >
-                  {t['Create']}
-                </Button>
-              </div>
             </div>
+          </div>
+          <div
+            className='dialog-action'
+            style={{ display: 'flex', gap: 25, justifyContent: 'flex-end' }}
+          >
+            <Button type='button' className='cancel' onClick={props.onClose}>
+              {t['Cancel']}
+            </Button>
+
+            <Button
+              disabled={name.length === 0 || description.length === 0}
+              className='primary sm'
+              onClick={handleCreateProject}
+            >
+              {t['Create']}
+            </Button>
           </div>
         </Dialog.Content>
       </Dialog.Portal>
