d42f935b002ec2f64678f24a046601af37067780
lorinjameson <lwjameson@gmail.com>
2024-11-22 12:58:32 -0500

    Fix for password reset

--- src/backend/supabaseBrowserClient.ts
+++ src/backend/supabaseBrowserClient.ts
@@ -1,10 +1,12 @@
-import { type SupabaseClient, createClient } from '@supabase/supabase-js';
+import { type SupabaseClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
 
 const SUPABASE = import.meta.env.PUBLIC_SUPABASE;
 const SUPABASE_API_KEY = import.meta.env.PUBLIC_SUPABASE_API_KEY;
 
-// Not ideal but prevents the browser client from being generated 
+// Not ideal but prevents the browser client from being generated
 // on the server
 // @ts-ignore
-export const supabase: SupabaseClient = 
-  typeof window !== 'undefined' && createClient(SUPABASE, SUPABASE_API_KEY);
+export const supabase: SupabaseClient =
+  typeof window !== 'undefined' &&
+  createBrowserClient(SUPABASE, SUPABASE_API_KEY);
