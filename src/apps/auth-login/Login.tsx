afa37d75f1a48d768f94be16b88542d3d922929f
lorinjameson <lwjameson@gmail.com>
2024-12-05 15:20:33 -0500

    SSO redirect

--- src/apps/auth-login/Login.tsx
+++ src/apps/auth-login/Login.tsx
@@ -36,6 +36,11 @@ export const Login = (props: {
   const [primary, ...loginMethods] = props.methods;
   const { t } = props.i18n;
 
+  const host =
+    window.location.port !== ''
+      ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
+      : `${window.location.protocol}//${window.location.hostname}`;
+
   const url = new URLSearchParams(window.location.search);
   let redirectUrl = url.get('redirect-to');
   if (redirectUrl) {
@@ -84,9 +89,7 @@ export const Login = (props: {
       .signInWithSSO({
         domain: domain,
         options: {
-          redirectTo: redirectUrl
-            ? redirectUrl
-            : `/${props.i18n.lang}/projects`,
+          redirectTo: `${host}/auth/callback`,
         },
       })
       .then(({ data, error }) => {
@@ -100,10 +103,6 @@ export const Login = (props: {
   };
 
   const signInWithKeycloak = () => {
-    const host =
-      window.location.port !== ''
-        ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}`
-        : `${window.location.protocol}//${window.location.hostname}`;
     supabase.auth
       .signInWithOAuth({
         provider: 'keycloak',
