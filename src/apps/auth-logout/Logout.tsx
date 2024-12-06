36c9e322d79594d47d64d4747fd93b2a4ce3f212
lorinjameson <lwjameson@gmail.com>
2024-12-06 14:54:09 -0500

    Redirect issue

--- src/apps/auth-logout/Logout.tsx
+++ src/apps/auth-logout/Logout.tsx
@@ -12,8 +12,6 @@ const clearCookies = () => {
   document.cookie = `sb-refresh-token=; path=/; expires=${expires}; SameSite=Lax; secure`;
 };
 
-localStorage.removeItem('redirect-to');
-
 export const Logout = (props: { i18n: Translations }) => {
   localStorage.removeItem('redirect-to');
   const arr = []; // Array to hold the keys
