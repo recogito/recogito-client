fca436dde0a14b59271500d821fc1bff5c4cca38
lorinjameson <lwjameson@gmail.com>
2024-12-06 13:57:32 -0500

    Fix for Keycloak

--- src/apps/auth-login/Login.tsx
+++ src/apps/auth-login/Login.tsx
@@ -4,7 +4,7 @@ import { Button } from '@components/Button';
 import classNames from 'classnames';
 import { useCallback, useEffect, useState } from 'react';
 import type { Session } from '@supabase/supabase-js';
-import { supabase } from '@backend/supabaseBrowserClient';
+import { supabase, supabaseImplicit } from '@backend/supabaseBrowserClient';
 import { isLoggedIn } from '@backend/auth';
 import type { LoginMethod, Translations } from 'src/Types';
 import { StateChecking, StateLoginForm } from './states';
