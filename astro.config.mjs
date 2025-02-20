3a731b656cc010ccee78e02f193af9b86af34e64
lorinjameson <lwjameson@gmail.com>
2025-02-20 15:25:10 -0500

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
+import gettyPlugin from '@recogito/plugin-reconciliation-service'
 
 export default defineConfig({
   integrations: [
-    react()
+    react(),
+    gettyPlugin()
   ],
   output: 'server',
   adapter: netlify(),
