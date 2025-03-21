45f6495d39f7aec353fd64b3cdf26232659e2582
lorinjameson <lwjameson@gmail.com>
2025-03-21 13:48:03 -0400

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify';
+import plugin3D from '@recogito/plugin-duke-unreal-export';
+import pluginReconciliation from '@recogito/plugin-reconciliation-service'
+
 
 export default defineConfig({
   integrations: [
-    react()
+    react(),
+    plugin3D(),
+    pluginReconciliation(),
   ],
   output: 'server',
   adapter: netlify(),
