0ba00c45cd1b8aeee801a2dbdac9a5423ff52dab
Rainer Simon <rainer@rainersimon.io>
2025-04-10 10:44:25 +0200

    Basic body crosswalking

--- src/util/iiif/parseManifestAnnotations.ts
+++ src/util/iiif/parseManifestAnnotations.ts
@@ -1,23 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
 import type { Manifest } from '@allmaps/iiif-parser';
-import { parseW3CImageAnnotation } from '@annotorious/annotorious';
+import { parseW3CImageAnnotation, type AnnotationBody } from '@annotorious/annotorious';
+import type { SupabaseAnnotation } from '@recogito/studio-sdk';
 
-/*
-const crosswalkBodies = (bodies: AnnotationBody[]) => {
+const crosswalkBodies = (bodies: AnnotationBody[]): AnnotationBody[] => {
   const keepPurposes = new Set(['commenting', 'replying', 'describing']);
 
   const crosswalkPurpose = (purpose?: string) =>
     (!purpose || purpose === 'describing') ? 'commenting' : purpose;
 
   const toKeep = bodies.filter(b => typeof b.value === 'string' && (!b.purpose || keepPurposes.has(b.purpose)));
-
   return toKeep.map(b => ({
     id: b.id || uuidv4(),
     purpose: crosswalkPurpose(b.purpose), 
     created: b.created,
-    value: JSON.stringify(generateJSON(b.value!, extensions))
-  }));
+    value: b.value
+  } as AnnotationBody));
 }
-  */
 
 export const parseManifestAnnotations = (manifest: Manifest) => {
 
@@ -27,12 +26,13 @@ export const parseManifestAnnotations = (manifest: Manifest) => {
       const items = ('items' in page ? page.items as any[] : []) || [];
 
       // TODO crosswalk items to SupabaseAnnotations
-      const crosswalked = items.map(item => {
-        return {
+      const crosswalked = items.map(item => ({
           layer_id: 'manifest',
           ...parseW3CImageAnnotation(item).parsed
-        }
-      });
+      })).map(a => ({
+        ...a,
+        bodies: crosswalkBodies(a.bodies || [])
+      } as SupabaseAnnotation));
 
       return [...all, ...crosswalked];
     }, []);
