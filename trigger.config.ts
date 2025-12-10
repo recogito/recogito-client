a23b8025e1147780a181abf1152beedab640fed7
dleadbetter <derek@performantsoftware.com>
2025-12-10 09:21:11 -0500

    #375 - Pulling Trigger.dev project ID out to environment variable

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from '@trigger.dev/sdk/v3';
+import 'dotenv/config';
 
 export default defineConfig({
-  project: 'proj_laigugtxozpzhsnwklfr',
+  project: process.env.TRIGGER_PROJECT_ID || '',
   runtime: 'node',
   logLevel: 'log',
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
