921c8e056fb9029760ba1cffb88bb257e21b96b7
Camden <c@camdenmecklem.com>
2026-08-28 12:18:50 -0400

    Add live updates to jobs table (#543)
    * add live updates to jobs table
    
    * loading state + more efficient queries
    
    * move refresh outside of effect
    
    * remove unneeded setLoading call
    
    * typo
    
    * better UI string
    
    * clear channel ref after removing channel
    
    * move outside of then block

--- src/apps/jobs-management/JobsManagement.tsx
+++ src/apps/jobs-management/JobsManagement.tsx
@@ -1,26 +1,109 @@
 import { JobsTable } from '@apps/jobs-management/JobsTable';
-import { deleteJob } from '@backend/crud';
+import { deleteJob, getJobs } from '@backend/crud';
 import { supabase } from '@backend/supabaseBrowserClient';
+import { Spinner } from '@components/Spinner';
 import { Toast, type ToastContent, ToastProvider } from '@components/Toast';
 import { TopBar } from '@components/TopBar';
 import { ArrowLeftIcon } from '@phosphor-icons/react';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { I18nextProvider, useTranslation } from 'react-i18next';
 import clientI18next from 'src/i18n/client';
 import type { Job, MyProfile } from 'src/Types';
 
 import './JobsManagement.css';
+import type { RealtimeChannel } from '@supabase/supabase-js';
 
 interface Props {
-  jobs: Job[];
   me: MyProfile;
 }
 
 const JobsManagement = (props: Props) => {
-  const [jobs, setJobs] = useState<Job[]>(props.jobs);
+  const [jobs, setJobs] = useState<Job[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
   const [toast, setToast] = useState<ToastContent | null>(null);
 
   const { t, i18n } = useTranslation(['jobs-management', 'common']);
+
+  const channel = useRef<RealtimeChannel>(null);
+
+  const refresh = useCallback(() => {
+    getJobs(supabase).then(({ error, data }) => {
+      if (error) {
+        setToast({
+          title: t('Something went wrong', { ns: 'common' }),
+          description: t('Could not load jobs.', { ns: 'jobs-management' }),
+          type: 'error',
+        });
+      } else {
+        setJobs(data);
+      }
+
+      setLoading(false);
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!channel.current) {
+      refresh()
+
+      channel.current = supabase
+        .channel('jobs-changes')
+        .on(
+          'postgres_changes',
+          {
+            event: 'UPDATE',
+            schema: 'public',
+            table: 'jobs',
+          },
+          ({ new: row }) => setJobs((prevJobs) => prevJobs.map((prevJob) => (
+            prevJob.id === row.id ? {
+              ...prevJob,
+              name: row.name,
+              job_status: row.job_status,
+              job_type: row.job_type,
+            } : prevJob
+          )))
+        )
+        .on(
+          'postgres_changes',
+          {
+            event: 'DELETE',
+            schema: 'public',
+            table: 'jobs',
+          },
+          ({ old }) => setJobs((prevJobs) => prevJobs.filter(
+            (prevJob) => prevJob.id !== old.id)
+          )
+        )
+        .on(
+          'postgres_changes',
+          {
+            event: 'INSERT',
+            schema: 'public',
+            table: 'jobs',
+          },
+          () => refresh()
+        )
+        .subscribe((status) => {
+          //   other statuses trigger on page refresh or unmount so TIMED_OUT
+          //   is the only one we can confidently display an error for
+          if (status === 'TIMED_OUT') {
+            setToast({
+              title: t('Something went wrong', { ns: 'common' }),
+              description: t('Could not fetch live updates from the server.', { ns: 'jobs-management' }),
+              type: 'error',
+            });
+          }
+        });
+    }
+
+    return () => {
+      if (channel.current) {
+        supabase.removeChannel(channel.current);
+        channel.current = null;
+      }
+    };
+  }, [refresh]);
 
   const onDelete = useCallback((job: Job) => (
     deleteJob(supabase, job.id).then(({ error }) => {
@@ -64,9 +147,13 @@ const JobsManagement = (props: Props) => {
         </div>
         <main className='jobs-management-content' id='main'>
           <div className='jobs-management-table'>
-            {jobs.length > 0 ? (
+            {loading ? (
+              <div className='jobs-management-loading'>
+                <Spinner />
+              </div>
+            ) : jobs.length > 0 ? (
               <JobsTable
-                jobs={props.jobs}
+                jobs={jobs}
                 onDelete={onDelete}
               />
             ) : (
